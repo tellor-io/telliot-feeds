1967c0929ca2f398a311fc08173109a80d07a074
oraclown <owen.burton13@gmail.com>
2021-11-11 13:22:50 +0000

    add report test

---
+++ tests/telliot_ampl_feeds/legacy_id_10/test_report_usd_vwap.py
@@ -0,0 +1,30 @@
+import pytest
+
+from telliot_ampl_feeds.feeds.usd_vwap import ampl_usd_vwap_feed
+from telliot_ampl_feeds.sources import uspce
+from telliot_feed_examples.reporters.interval import IntervalReporter
+
+
+@pytest.mark.asyncio
+async def test_uspce_reporter_submit_once(cfg, master, oracle):
+    """Test reporting AMPL/USD/VWAP to the TellorX Oracle on Rinkeby."""
+    # Override Python built-in input method
+    uspce.input = lambda: "123.456"
+
+    rinkeby_endpoint = cfg.get_endpoint()
+
+    reporter = IntervalReporter(
+        endpoint=rinkeby_endpoint,
+        private_key=cfg.main.private_key,
+        master=master,
+        oracle=oracle,
+        datafeeds=[ampl_usd_vwap_feed],
+    )
+
+    tx_receipts = await reporter.report_once()
+
+    assert tx_receipts is not None
+
+    for receipt in tx_receipts:
+        assert isinstance(receipt, tuple)
+        assert receipt[0].to == oracle.address
