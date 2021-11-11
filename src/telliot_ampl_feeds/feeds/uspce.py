30617131104743d031cc2cf2cd84d3e8c7d6e527
oraclown <owen.burton13@gmail.com>
2021-11-11 13:45:31 +0000

    change to correct legacy id

--- src/telliot_ampl_feeds/feeds/uspce.py
+++ src/telliot_ampl_feeds/feeds/uspce.py
@@ -5,4 +5,4 @@ from telliot_core.queries.legacy_query import LegacyRequest
 from telliot_ampl_feeds.sources.uspce import USPCESource
 
 
-uspce_feed = DataFeed(query=LegacyRequest(legacy_id=10), source=USPCESource())
+uspce_feed = DataFeed(query=LegacyRequest(legacy_id=41), source=USPCESource())
