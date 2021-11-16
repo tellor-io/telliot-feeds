2db848fec75ed68ade017ccbf42f5f1b528ab1d9
oraclown <owen.burton13@gmail.com>
2021-11-16 18:52:56 +0000

    remove unneeded scripts

--- scripts/get_default_configs.py
+++
@@ -1,4 +0,0 @@
-from telliot_core.apps.telliot_config import TelliotConfig
-
-# Generate default configuration files
-_ = TelliotConfig()
--- scripts/report_id_10.sh
+++
@@ -1,2 +0,0 @@
-# report AMPL/USD/VWAP every day, 15 min after midnight
-cd ~/telliot-feed-examples/src/ && python -m telliot_ampl_feeds.scripts.report_usd_vwap
