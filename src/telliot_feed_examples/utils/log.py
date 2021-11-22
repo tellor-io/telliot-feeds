e5b923a71720e4d9d17906e0d390edbeffb727a1
oraclown <owen.burton13@gmail.com>
2021-11-22 11:03:11 +0000

    add logger

---
+++ src/telliot_feed_examples/utils/log.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+from telliot_core.utils.home import default_homedir
+
+
+def get_logger(name: str) -> logging.Logger:
+    '''Telliot feed examples logger
+
+    Returns a logger that logs to stdout and file. The name arg
+    should be the current file name. For example:
+    _ = get_logger(name=__name__)
+    '''
+    logger = logging.getLogger(name)
+
+    logs_file = default_homedir() / ("telliot-feed-examples.log")
+    logs_file = logs_file.resolve().absolute()
+
+    output_file_handler = logging.FileHandler(logs_file)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    output_file_handler.setFormatter(formatter)
+    stdout_handler.setFormatter(formatter)
+
+    logger.addHandler(output_file_handler)
+    logger.addHandler(stdout_handler)
+
+    return logger
---
+++ tests/utils/test_logging.py
@@ -0,0 +1,15 @@
+from telliot_feed_examples.utils.log import get_logger
+from telliot_core.utils.home import default_homedir
+from logging import Logger
+
+
+def test_get_logger() -> None:
+    '''Tests instantiating a logger for the main package.'''
+    logger = get_logger(__name__)
+    log_file = logger.handlers[0].baseFilename
+    expected_log_file = default_homedir() / ('telliot-feed-examples.log')
+    expected_log_file.resolve().absolute()
+
+    assert isinstance(logger, Logger)
+    assert log_file == str(expected_log_file)
+    assert logger.name == __name__
