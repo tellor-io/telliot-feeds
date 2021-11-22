41a1f542535326ddd0fd3d50f4180d160062f858
oraclown <owen.burton13@gmail.com>
2021-11-22 11:53:18 +0000

    change logging level

--- src/telliot_feed_examples/utils/log.py
+++ src/telliot_feed_examples/utils/log.py
@@ -1,16 +1,19 @@
 import logging
 import sys
+
 from telliot_core.utils.home import default_homedir
 
 
 def get_logger(name: str) -> logging.Logger:
-    '''Telliot feed examples logger
+    """Telliot feed examples logger
 
     Returns a logger that logs to stdout and file. The name arg
     should be the current file name. For example:
     _ = get_logger(name=__name__)
-    '''
+    """
     logger = logging.getLogger(name)
+
+    logger.setLevel(level=logging.INFO)
 
     logs_file = default_homedir() / ("telliot-feed-examples.log")
     logs_file = logs_file.resolve().absolute()
@@ -18,7 +21,9 @@ def get_logger(name: str) -> logging.Logger:
     output_file_handler = logging.FileHandler(logs_file)
     stdout_handler = logging.StreamHandler(sys.stdout)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     output_file_handler.setFormatter(formatter)
     stdout_handler.setFormatter(formatter)
 
