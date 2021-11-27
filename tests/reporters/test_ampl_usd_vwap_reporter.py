68f0aa0035119288fd794ffded5852ab09f58f6d
oraclown <owen.burton13@gmail.com>
2021-11-27 13:40:51 +0100

    remove comment

--- tests/reporters/test_ampl_usd_vwap_reporter.py
+++ tests/reporters/test_ampl_usd_vwap_reporter.py
@@ -4,7 +4,6 @@ from telliot_feed_examples.feeds.ampl_usd_vwap_feed import ampl_usd_vwap_feed
 from tests.conftest import reporter_submit_once
 
 
-# @pytest.mark.skip("uninvestigated error")
 @pytest.mark.asyncio
 async def test_uspce_reporter_submit_once(rinkeby_cfg, master, oracle):
     """Test reporting AMPL/USD/VWAP to the TellorX Oracle on Rinkeby."""
