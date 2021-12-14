4d1f5718499362118baa53dc3c0bc4c3de6500d0
oraclown <owen.burton13@gmail.com>
2021-12-14 06:41:07 -0800

    update feed

--- src/telliot_feed_examples/feeds/aws_ec2_feed.py
+++ src/telliot_feed_examples/feeds/aws_ec2_feed.py
@@ -1,1 +1,10 @@
 "Example AWS EC2 spot price feeds."
+from telliot_core.datafeed import DataFeed
+from telliot_core.queries.string_query import StringQuery
+
+from telliot_feed_examples.sources.aws_ec2 import AWSEC2PriceSource
+
+aws_ec2_us_east_1_feed = DataFeed(
+    query=StringQuery(text="AWS EC2 spot prices for us-east-1?"),
+    source=AWSEC2PriceSource(region="us-east-1"),
+)
