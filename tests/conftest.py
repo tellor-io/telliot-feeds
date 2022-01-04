1d5c789b5176d1f4f043acb961e388274e60a93d
oraclown <owen.burton13@gmail.com>
2022-01-03 18:00:31 -0800

    Fix interval reporter tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,9 @@
 import os
 
 import pytest
-from telliot_core.apps.core import TelliotCore
 from telliot_core.apps.telliot_config import TelliotConfig
 from telliot_core.datasource import DataSource
 from telliot_core.types.datapoint import OptionalDataPoint
-from web3.datastructures import AttributeDict
-
-from telliot_feed_examples.feeds.uspce_feed import uspce_feed
-from telliot_feed_examples.reporters.interval import IntervalReporter
-from telliot_feed_examples.sources import uspce
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -41,74 +35,6 @@ def rinkeby_cfg():
         rinkeby_staker.address = "0x8D8D2006A485FA4a75dFD8Da8f63dA31401B8fA2"
 
     return cfg
-
-
-@pytest.fixture()
-def rinkeby_core(rinkeby_cfg):
-
-    app = TelliotCore.get() or TelliotCore(config=rinkeby_cfg)
-
-    # Replace staker private key
-    staker = app.get_default_staker()
-    if os.getenv("PRIVATE_KEY", None):
-        staker.private_key = os.getenv("PRIVATE_KEY", None)
-        staker.address = "0x8D8D2006A485FA4a75dFD8Da8f63dA31401B8fA2"
-
-    app.startup()
-    yield app
-
-    # Destroy app instance after test (not working)
-    TelliotCore.destroy()
-
-
-EXPECTED_ERRORS = {
-    "Current addess disputed. Switch address to continue reporting.",
-    "Current address is locked in dispute or for withdrawal.",
-    "Current address is in reporter lock.",
-    "Estimated profitability below threshold.",
-    "Estimated gas price is above maximum gas price.",
-    "Unable to retrieve updated datafeed value.",
-}
-
-
-async def reporter_submit_once(rinkeby_core, feed):
-    """Test reporting once to the TellorX playground on Rinkeby
-    with three retries."""
-
-    rinkeby_endpoint = rinkeby_core.config.get_endpoint()
-
-    if feed == uspce_feed:
-        # Override Python built-in input method
-        uspce.input = lambda: "123.456"
-
-    private_key = rinkeby_core.get_default_staker().private_key
-
-    user = rinkeby_endpoint.web3.eth.account.from_key(private_key).address
-    last_timestamp, read_status = await rinkeby_core.tellorx.oracle.read(
-        "getReporterLastTimestamp", _reporter=user
-    )
-    assert read_status.ok
-
-    reporter = IntervalReporter(
-        endpoint=rinkeby_endpoint,
-        private_key=private_key,
-        master=rinkeby_core.tellorx.master,
-        oracle=rinkeby_core.tellorx.oracle,
-        datafeed=feed,
-    )
-
-    tx_receipt, status = await reporter.report_once()
-
-    # Reporter submitted
-    if tx_receipt is not None and status.ok:
-        assert isinstance(tx_receipt, AttributeDict)
-        assert tx_receipt.to == rinkeby_core.tellorx.oracle.address
-    # Reporter did not submit
-    else:
-        assert not tx_receipt
-        assert not status.ok
-        assert status.error in EXPECTED_ERRORS
-        print(status.error)
 
 
 @pytest.fixture(scope="session")
--- tests/reporters/test_interval_reporter.py
+++ tests/reporters/test_interval_reporter.py
@@ -5,13 +5,12 @@ telliot's reporters subpackage.
 import pytest
 from telliot_core.apps.core import TelliotCore
 from telliot_core.utils.response import ResponseStatus
+from web3.datastructures import AttributeDict
 
 from telliot_feed_examples.feeds.eth_usd_feed import eth_usd_median_feed
 from telliot_feed_examples.reporters.interval import IntervalReporter
-from tests.conftest import reporter_submit_once
 
 
-@pytest.mark.skip
 @pytest.fixture
 async def eth_usd_reporter(rinkeby_cfg):
     """Returns an instance of an IntervalReporter using
@@ -36,73 +35,97 @@ async def eth_usd_reporter(rinkeby_cfg):
         return r
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
 async def test_ensure_staked(eth_usd_reporter):
     """Test staking status of reporter."""
-    gp = eth_usd_reporter.gas_price
-
-    staked, status = await eth_usd_reporter.ensure_staked(gas_price_gwei=gp)
+    staked, status = await eth_usd_reporter.ensure_staked()
 
     assert staked
     assert status.ok
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
 async def test_check_reporter_lock(eth_usd_reporter):
     """Test checking if in reporter lock."""
     r = eth_usd_reporter
+    r.last_submission_timestamp = 0
 
-    assert r.last_submission_timestamp == 0
+    status = await r.check_reporter_lock()
 
-    reporter_locked, status = await r.check_reporter_lock()
-
-    assert reporter_locked
-    assert not status.ok
-    assert status.error == "Current address is in reporter lock."
-    assert r.last_submission_timestamp != 0
-
-
-@pytest.mark.skip
-@pytest.mark.asyncio
-async def test_enforce_gas_price_limit(eth_usd_reporter):
-    """Test max gas price limit."""
-    r = eth_usd_reporter
-
-    assert r.max_gas_price == 0
-
-    r.max_gas_price = 10
-    gas_price_below_limit, status = await r.enforce_gas_price_limit(gas_price_gwei=1e10)
-
-    assert not gas_price_below_limit
-    assert not status.ok
-    assert status.error == "Estimated gas price is above maximum gas price."
+    assert isinstance(status, ResponseStatus)
+    if not status.ok:
+        assert status.error == "Current address is in reporter lock."
+        assert r.last_submission_timestamp != 0
 
 
-@pytest.mark.skip
 @pytest.mark.asyncio
 async def test_ensure_profitable(eth_usd_reporter):
     """Test profitability check."""
     r = eth_usd_reporter
-    gp = r.gas_price
 
-    assert r.profit_threshold == 0
+    assert r.expected_profit == "YOLO"
 
-    profitable, status = await r.ensure_profitable(gas_price_gwei=gp)
+    status = await r.ensure_profitable()
 
-    assert profitable
     assert status.ok
 
-    r.profit_threshold = 1e10
-    profitable, status = await r.ensure_profitable(gas_price_gwei=gp)
+    r.expected_profit = 1e10
+    status = await r.ensure_profitable()
 
-    assert not profitable
     assert not status.ok
     assert status.error == "Estimated profitability below threshold."
 
 
-@pytest.mark.skip
+@pytest.mark.asyncio
+async def test_fetch_gas_price(eth_usd_reporter):
+    """Test retrieving custom gas price from eth gas station."""
+    r = eth_usd_reporter
+
+    assert r.gas_price_speed == "safeLow"
+
+    r.gas_price_speed = "average"
+
+    assert r.gas_price_speed != "safeLow"
+
+    gas_price = await r.fetch_gas_price()
+
+    assert isinstance(gas_price, int)
+    assert gas_price > 0
+
+
+@pytest.mark.asyncio
+async def test_interval_reporter_submit_once(eth_usd_reporter):
+    """Test reporting once to the TellorX playground on Rinkeby
+    with three retries."""
+    r = eth_usd_reporter
+
+    EXPECTED_ERRORS = {
+        "Current addess disputed. Switch address to continue reporting.",
+        "Current address is locked in dispute or for withdrawal.",
+        "Current address is in reporter lock.",
+        "Estimated profitability below threshold.",
+        "Estimated gas price is above maximum gas price.",
+        "Unable to retrieve updated datafeed value.",
+    }
+
+    ORACLE_ADDRESSES = {
+        "0xe8218cACb0a5421BC6409e498d9f8CC8869945ea",  # mainnet
+        "0x18431fd88adF138e8b979A7246eb58EA7126ea16",  # rinkeby
+    }
+
+    tx_receipt, status = await r.report_once()
+
+    # Reporter submitted
+    if tx_receipt is not None and status.ok:
+        assert isinstance(tx_receipt, AttributeDict)
+        assert tx_receipt.to in ORACLE_ADDRESSES
+    # Reporter did not submit
+    else:
+        assert not tx_receipt
+        assert not status.ok
+        assert status.error in EXPECTED_ERRORS
+
+
 @pytest.mark.asyncio
 async def test_no_updated_value(eth_usd_reporter, bad_source):
     """Test handling for no updated value returned from datasource."""
@@ -117,39 +140,18 @@ async def test_no_updated_value(eth_usd_reporter, bad_source):
 
     # Override reporter lock status
     async def passing():
-        return False, ResponseStatus()
+        return ResponseStatus()
 
     r.check_reporter_lock = passing
+
+    # Override reporter profit check
+    async def profit():
+        return ResponseStatus()
+
+    r.ensure_profitable = profit
 
     tx_receipt, status = await r.report_once()
 
     assert not tx_receipt
     assert not status.ok
     assert status.error == "Unable to retrieve updated datafeed value."
-
-
-@pytest.mark.skip
-@pytest.mark.asyncio
-async def test_fetch_gas_price(eth_usd_reporter):
-    """Test retrieving custom gas price from eth gas station."""
-    r = eth_usd_reporter
-
-    assert r.gas_price_speed == "fast"
-
-    r.gas_price_speed = "safeLow"
-
-    assert r.gas_price_speed != "fast"
-
-    gas_price = await r.fetch_gas_price()
-
-    assert isinstance(gas_price, int)
-    assert gas_price > 0
-
-
-@pytest.mark.skip
-@pytest.mark.asyncio
-async def test_interval_reporter_submit_once(rinkeby_core):
-    """Test reporting once to the TellorX playground on Rinkeby
-    with three retries."""
-
-    await reporter_submit_once(rinkeby_core, eth_usd_median_feed)
