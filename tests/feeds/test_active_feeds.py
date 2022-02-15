9f036f862749015d1ac27cee4fae6590b71fad85
oraclown <owen.burton13@gmail.com>
2022-02-15 10:00:33 -0800

    Add test

---
+++ tests/feeds/test_active_feeds.py
@@ -0,0 +1,9 @@
+from telliot_feed_examples.feeds import CATALOG_FEEDS
+from telliot_core.data.query_catalog import query_catalog
+
+
+def test_supports_all_active_queries():
+    """Make sure all current queries have an associated feed that reporters can use."""
+    active_q_tags = [q.tag for q in query_catalog.find()]
+    for qt in active_q_tags:
+        assert qt in CATALOG_FEEDS
