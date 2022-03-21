cd86a65f0a665e91af2dc0e28cc3f296e47a18ce
oraclown <owen.burton13@gmail.com>
2022-03-21 13:53:27 -0400

    Fix style errs

--- tests/utils/test_historical_price_source_utils.py
+++ tests/utils/test_historical_price_source_utils.py
@@ -6,8 +6,8 @@ from telliot_feed_examples.sources.price.historical.utils import ensure_valid_ti
 def test_ensure_valid_timestamp():
     """Make sure timestamp not too soon."""
     too_soon_ts = int(time.time())
-    period = 1234 # seconds
+    period = 1234  # seconds
     adjusted_ts = ensure_valid_timestamp(too_soon_ts, period)
 
     assert adjusted_ts < too_soon_ts
-    assert adjusted_ts + period/2 < time.time()
+    assert adjusted_ts + period / 2 < time.time()
