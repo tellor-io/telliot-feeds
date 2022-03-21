6d21df52c70398d620a0b9e1e771aaab4c0bf015
oraclown <owen.burton13@gmail.com>
2022-03-21 09:43:16 -0400

    Generate csv for kraken historical price data

---
+++ scripts/generate_price_history_csv.py
@@ -0,0 +1,37 @@
+import asyncio
+import csv
+
+from telliot_feed_examples.sources.price.historical.kraken import (
+    KrakenHistoricalPriceService,
+)
+
+
+def get_trades():
+    six_hours = 60 * 60 * 6 # seconds
+    trades, _ = asyncio.run(KrakenHistoricalPriceService().get_trades(
+        "eth",
+        "usd",
+        period=six_hours,
+        ts=1647782323,
+    ))
+    print("# trades in six hour window:", len(trades))
+    return trades
+
+
+def generate_csv(trades: list) -> None:
+    # source: https://docs.kraken.com/rest/#operation/getRecentTrades
+    cols = ["price", "volume", "time", "buy/sell", "market/limit", "miscellaneous"]
+    
+    with open('kraken_historical_price_source', 'w') as f:
+        write = csv.writer(f)
+        write.writerow(cols)
+        write.writerows(trades)
+
+
+def main():
+    trades = get_trades()
+    generate_csv(trades)
+
+
+if __name__ == "__main__":
+    main()
