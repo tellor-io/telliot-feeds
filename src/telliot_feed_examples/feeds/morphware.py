8f2d3801796d648410305b2ac9df1ae8fe949fa2
oraclown <owen.burton13@gmail.com>
2022-04-14 11:53:33 -0400

    Add Morphware V1 DataFeed

---
+++ src/telliot_feed_examples/feeds/morphware.py
@@ -0,0 +1,10 @@
+"""Datafeed for reporting responses to the Morphware query type.
+
+More info: """
+from telliot_core.datafeed import DataFeed
+from telliot_core.queries.morphware import Morphware
+
+from telliot_feed_examples.sources.morphware import MorphwareV1Source
+
+
+morphware_v1_feed = DataFeed(query=Morphware(version=1), source=MorphwareV1Source())
