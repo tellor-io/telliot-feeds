8faeceb238348de8a855b3eb988e67b6ba301d56
Tim Kernell <tpkernell@gmail.com>
2022-04-19 10:20:24 -0400

    draft rng feed example

--- src/telliot_feed_examples/sources/blockhash_aggregator.py
+++ src/telliot_feed_examples/sources/blockhash_aggregator.py
@@ -1,19 +1,25 @@
 from dataclasses import dataclass
-from web3 import Web3
-import requests
-from typing import Any
 from datetime import datetime
 from datetime import timezone
+from typing import Any
+from typing import Optional
 
+import requests
 from telliot_core.datasource import DataSource
 from telliot_core.dtypes.datapoint import DataPoint
+from web3 import Web3
 
 from telliot_feed_examples.utils.log import get_logger
 
 
 logger = get_logger(__name__)
 
-w3 = Web3(Web3.HTTPProvider('https://eth-mainnet.alchemyapi.io/v2/hP3lNPFpxPSwfwJtfaZi4ezZlPgimAnN'))
+w3 = Web3(
+    Web3.HTTPProvider(
+        "https://eth-mainnet.alchemyapi.io/v2/hP3lNPFpxPSwfwJtfaZi4ezZlPgimAnN"
+    )
+)
+
 
 @dataclass
 class TellorRNGManualSource(DataSource[Any]):
@@ -23,9 +29,7 @@ class TellorRNGManualSource(DataSource[Any]):
 
     def parse_user_val(self) -> int:
         """Parse timestamp from user input."""
-        print(
-            "Enter timestamp for generating a random number: "
-        )
+        print("Enter timestamp for generating a random number: ")
 
         data = None
         while data is None:
@@ -37,7 +41,10 @@ class TellorRNGManualSource(DataSource[Any]):
                 print("Invalid input. Enter decimal value (int).")
                 continue
 
-            print(f"Generating random number from timestamp: {inpt}\nPress [ENTER] to confirm.")
+            print(
+                "Generating random number from timestamp: "
+                f"{inpt}\nPress [ENTER] to confirm."
+            )
             _ = input()
             data = inpt
 
@@ -45,41 +52,49 @@ class TellorRNGManualSource(DataSource[Any]):
         self.timestamp = data
         return data
 
-    def getEthHashByTimestamp(self, timestamp) -> str:
-        this_block = w3.eth.get_block('latest')
-        if(this_block.timestamp < timestamp):
-            return str('')
+    def getEthHashByTimestamp(self, timestamp: int) -> str:
+        this_block = w3.eth.get_block("latest")
+        if this_block is None:
+            return ""
+        if this_block["timestamp"] < timestamp:
+            return ""
         else:
-            min_num = 0
-            max_num = this_block.number
-            mid_num = 0
-            while(max_num - min_num > 1):
-                mid_num = round((max_num + min_num)/2)
+            min_num: int = 0
+            max_num: int = this_block["number"]
+            mid_num: int = 0
+            while max_num - min_num > 1:
+                mid_num = round((max_num + min_num) / 2)
                 this_block = w3.eth.get_block(mid_num)
-                if(this_block.timestamp > timestamp):
+                if this_block["timestamp"] > timestamp:
                     max_num = mid_num
                 else:
                     min_num = mid_num
             this_block = w3.eth.get_block(max_num)
-            return(this_block.hash.hex())
+            return str(this_block["hash"].hex())
 
-    def getBtcHashByTimestamp(self, timestamp) -> str:
-        this_block = requests.get('https://blockchain.info/latestblock').json()
-        if(this_block['time'] < timestamp):
-            return str('')
+    def getBtcHashByTimestamp(self, timestamp: int) -> str:
+        this_block = requests.get("https://blockchain.info/latestblock").json()
+        if this_block is None:
+            return ""
+        if this_block["time"] < timestamp:
+            return ""
         else:
-            min_num = 0
-            max_num = this_block['height']
-            mid_num = 0
-            while(max_num - min_num > 1):
-                mid_num = round((max_num + min_num)/2)
-                this_block = requests.get('https://blockchain.info/rawblock/' + str(mid_num)).json()
-                if(this_block['time'] > timestamp):
+            min_num: int = 0
+            max_num: int = this_block["height"]
+            mid_num: int = 0
+            while max_num - min_num > 1:
+                mid_num = round((max_num + min_num) / 2)
+                this_block = requests.get(
+                    "https://blockchain.info/rawblock/" + str(mid_num)
+                ).json()
+                if this_block["time"] > timestamp:
                     max_num = mid_num
                 else:
                     min_num = mid_num
-            this_block = requests.get('https://blockchain.info/rawblock/' + str(max_num)).json()
-            return(this_block['hash'])
+            this_block = requests.get(
+                "https://blockchain.info/rawblock/" + str(max_num)
+            ).json()
+            return str(this_block["hash"])
 
     async def fetch_new_datapoint(self) -> DataPoint[bytes]:
         """Update current value with time-stamped value fetched from user input.
@@ -87,18 +102,19 @@ class TellorRNGManualSource(DataSource[Any]):
         Returns:
             Current time-stamped value
         """
-        eth_hash = ''
-        btc_hash = ''
+        eth_hash: str = ""
+        btc_hash: str = ""
 
-        while len(eth_hash) == 0 and len(btc_hash) == 0:
-            if(self.timestamp == 0):
+        # while len(eth_hash) == 0 and len(btc_hash) == 0:
+        while eth_hash == "" and btc_hash == "":
+            if self.timestamp == 0:
                 timestamp = self.parse_user_val()
             else:
                 timestamp = self.timestamp
             eth_hash = self.getEthHashByTimestamp(timestamp)
             btc_hash = self.getBtcHashByTimestamp(timestamp)
 
-        data = Web3.solidityKeccak(['string', 'string'],[eth_hash, str(btc_hash)])
+        data = Web3.solidityKeccak(["string", "string"], [eth_hash, str(btc_hash)])
         dt = datetime.fromtimestamp(self.timestamp, tz=timezone.utc)
         datapoint = (data, dt)
 
---
+++ tests/sources/test_tellor_rng_source.py
@@ -0,0 +1,22 @@
+import pytest
+
+from telliot_feed_examples.sources import blockhash_aggregator
+from telliot_feed_examples.sources.blockhash_aggregator import TellorRNGManualSource
+
+# from telliot_feed_examples.feeds.tellor_rng_feed import tellor_rng_feed
+
+
+@pytest.mark.asyncio
+async def test_rng():
+    """Retrieve random number."""
+    blockhash_aggregator.input = lambda: "1649769707"
+    rng_source = TellorRNGManualSource()
+    v, t = await rng_source.fetch_new_datapoint()
+
+    assert v == (
+        b"Ad\x81\xc2\x9d\xab\x8a\xf6\x8fN^\xcd\xb9g\xdf{"
+        b"\xeap\xe4\xf8\xf2\xab\x89\xcb\xb0\xe6\x8cGR\x18\xf2+"
+    )
+
+    assert isinstance(v, float)
+    assert isinstance(timestamp, datetime)
