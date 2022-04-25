184e95b9d56303804ca889f6e01a7fbaa3b4466a
tkernell <tpkernell@gmail.com>
2022-04-25 09:59:01 -0400

    error handling for net requests

--- src/telliot_feed_examples/sources/blockhash_aggregator.py
+++ src/telliot_feed_examples/sources/blockhash_aggregator.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from datetime import timezone
 from re import L
 from typing import Any
+from typing import Optional
 
 import requests
 from telliot_core.datasource import DataSource
@@ -41,60 +42,78 @@ class TellorRNGManualSource(DataSource[Any]):
             _ = input()
             data = inpt
 
-        # data = 1649769707
         self.timestamp = data
         return data
 
-    def getEthHashByTimestamp(self, timestamp: int) -> str:
+    def getEthHashByTimestamp(self, timestamp: int) -> Optional[str]:
+        """Fetches next Ethereum blockhash after timestamp from API."""
+        
         cfg = TelliotConfig()
         # Override configuration for ethereum mainnet
         cfg.main.chain_id = 1
         cfg.get_endpoint().connect()
-        # w3 = Web3(Web3.WebsocketProvider(cfg.get_endpoint().url, websocket_timeout=60))
         w3 = cfg.get_endpoint().web3
 
-        this_block = w3.eth.get_block("latest")
-        if this_block is None:
-            return ""
-        if this_block["timestamp"] < timestamp:
-            return ""
-        else:
-            min_num: int = 0
-            max_num: int = this_block["number"]
-            mid_num: int = 0
-            while max_num - min_num > 1:
-                mid_num = round((max_num + min_num) / 2)
-                this_block = w3.eth.get_block(mid_num)
-                if this_block["timestamp"] > timestamp:
-                    max_num = mid_num
-                else:
-                    min_num = mid_num
-            this_block = w3.eth.get_block(max_num)
-            return str(this_block["hash"].hex())
+        # with cfg.get_endpoint().web3 as w3:
+        try:
+            this_block = w3.eth.get_block("latest")
+            if this_block is None:
+                return None
+            if this_block["timestamp"] < timestamp:
+                return None
+            else:
+                min_num: int = 0
+                max_num: int = this_block["number"]
+                mid_num: int = 0
+                while max_num - min_num > 1:
+                    mid_num = round((max_num + min_num) / 2)
+                    this_block = w3.eth.get_block(mid_num)
+                    if this_block is None:
+                        return None
+                    if this_block["timestamp"] > timestamp:
+                        max_num = mid_num
+                    else:
+                        min_num = mid_num
+                this_block = w3.eth.get_block(max_num)
+                if this_block is None:
+                    return None
+                return str(this_block["hash"].hex())
+        except Exception as e:
+            logger.error(f"Tellor RNG V1 ethereum API error: {e}")
+            return None
 
-    def getBtcHashByTimestamp(self, timestamp: int) -> str:
-        this_block = requests.get("https://blockchain.info/latestblock").json()
-        if this_block is None:
-            return ""
-        if this_block["time"] < timestamp:
-            return ""
-        else:
-            min_num: int = 0
-            max_num: int = this_block["height"]
-            mid_num: int = 0
-            while max_num - min_num > 1:
-                mid_num = round((max_num + min_num) / 2)
-                this_block = requests.get(
-                    "https://blockchain.info/rawblock/" + str(mid_num)
-                ).json()
-                if this_block["time"] > timestamp:
-                    max_num = mid_num
+    def getBtcHashByTimestamp(self, timestamp: int) -> Optional[str]:
+        """Fetches next Bitcoin blockhash after timestamp from API."""
+
+        with requests.Session() as s:
+            try: 
+                this_block = s.get("https://blockchain.info/latestblock").json()
+                if this_block is None:
+                    logger.error(f"Tellor RNG V1 no latest btc block returned from API")
+                    return ""
+                if this_block["time"] < timestamp:
+                    logger.error(f"Tellor RNG V1 current btc block time, {this_block['time']}, is less than given timestamp {timestamp}")
+                    return ""
                 else:
-                    min_num = mid_num
-            this_block = requests.get(
-                "https://blockchain.info/rawblock/" + str(max_num)
-            ).json()
-            return str(this_block["hash"])
+                    min_num: int = 0
+                    max_num: int = this_block["height"]
+                    mid_num: int = 0
+                    while max_num - min_num > 1:
+                        mid_num = round((max_num + min_num) / 2)
+                        this_block = s.get(f"https://blockchain.info/rawblock/{mid_num}").json()
+                        if this_block is None:
+                            return None
+                        if this_block["time"] > timestamp:
+                            max_num = mid_num
+                        else:
+                            min_num = mid_num
+                    this_block = s.get(f"https://blockchain.info/rawblock/{max_num}").json()
+                    if this_block is None:
+                        return None
+                    return str(this_block["hash"])
+            except requests.exceptions.RequestException as e:
+                logger.error(f"Tellor RNG V1 bitcoin API error: {e}")
+                return ""
 
     async def fetch_new_datapoint(self) -> DataPoint[bytes]:
         """Update current value with time-stamped value fetched from user input.
@@ -102,17 +121,20 @@ class TellorRNGManualSource(DataSource[Any]):
         Returns:
             Current time-stamped value
         """
-        eth_hash: str = ""
-        btc_hash: str = ""
 
-        # while len(eth_hash) == 0 and len(btc_hash) == 0:
-        while eth_hash == "" and btc_hash == "":
-            if self.timestamp == 0:
-                timestamp = self.parse_user_val()
-            else:
-                timestamp = self.timestamp
-            eth_hash = self.getEthHashByTimestamp(timestamp)
-            btc_hash = self.getBtcHashByTimestamp(timestamp)
+        if self.timestamp == 0:
+            timestamp = self.parse_user_val()
+        else:
+            timestamp = self.timestamp
+        eth_hash = self.getEthHashByTimestamp(timestamp)
+        btc_hash = self.getBtcHashByTimestamp(timestamp)
+        
+        if eth_hash is None:
+            logger.warning("No response from TellorRNG V1 Ethereum API")
+            return None, None
+        if btc_hash is None:
+            logger.warning("No response from TellorRNG V1 Bitcoin API")
+            return None, None
 
         data = Web3.solidityKeccak(["string", "string"], [eth_hash, str(btc_hash)])
         dt = datetime.fromtimestamp(self.timestamp, tz=timezone.utc)
--- tests/sources/test_tellor_rng_source.py
+++ tests/sources/test_tellor_rng_source.py
@@ -20,3 +20,19 @@ async def test_rng():
 
     assert isinstance(v, bytes)
     assert isinstance(t, datetime)
+
+@pytest.mark.asyncio
+async def test_rng_btc_source_error():
+    """Retrieve random number."""
+    blockhash_aggregator.input = lambda: "1649769707"
+    
+    rng_source = TellorRNGManualSource()
+    v, t = await rng_source.fetch_new_datapoint()
+
+    assert v == (
+        b"Ad\x81\xc2\x9d\xab\x8a\xf6\x8fN^\xcd\xb9g\xdf{"
+        b"\xeap\xe4\xf8\xf2\xab\x89\xcb\xb0\xe6\x8cGR\x18\xf2+"
+    )
+
+    assert isinstance(v, bytes)
+    assert isinstance(t, datetime)
