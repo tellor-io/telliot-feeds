3347930e377cd669e59551c3832c03cc2c83dd37
tallywiesenberg <t.j.wies@gmail.com>
2022-04-27 14:17:44 -0400

    style

--- tests/sources/test_gas_price_oracle_source.py
+++ tests/sources/test_gas_price_oracle_source.py
@@ -1,9 +1,8 @@
-
 from datetime import datetime
 from time import time
-from telliot_core.apps.telliot_config import TelliotConfig
 
 import pytest
+from telliot_core.apps.telliot_config import TelliotConfig
 
 from telliot_feed_examples.sources.gas_price_oracle import GasPriceOracleSource
 
@@ -17,10 +16,13 @@ async def test_fetch_new_datapoint():
     timestamp = time() - 2000
     chain_id = 1
 
-    v, t = await GasPriceOracleSource().fetch_new_datapoint(api_key, timestamp, chain_id)
+    v, t = await GasPriceOracleSource().fetch_new_datapoint(
+        api_key, timestamp, chain_id
+    )
 
     assert v is not None and t is not None
     assert isinstance(v, float)
     assert isinstance(t, datetime)
+
 
 EXAMPLE_RESPONSE = 31.8
