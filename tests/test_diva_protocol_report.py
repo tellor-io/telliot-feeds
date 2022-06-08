110243c40974f7841904d6c5280808ebcfb7973d
oraclown <owen.burton13@gmail.com>
2022-06-08 16:24:25 +0200

    Swap feed and local cfg

--- tests/test_diva_protocol_report.py
+++ tests/test_diva_protocol_report.py
@@ -2,35 +2,29 @@
 # from brownie import accounts
 # from telliot_core.apps.core import TelliotCore
 # from web3.datastructures import AttributeDict
-
-# from telliot_feed_examples.feeds.dai_usd_feed import dai_usd_median_feed
+# from telliot_feed_examples.feeds.diva_protocol_feed import assemble_diva_datafeed
 # from telliot_feed_examples.reporters.tellorflex import TellorFlexReporter
-
-
 # @pytest.mark.asyncio
-# async def test_dai_usd_reporter_submit_once(
-#     mumbai_test_cfg, mock_flex_contract, mock_autopay_contract, mock_token_contract
+# async def test_diva_protocol_reporter_submit_once(
+#     ropsten_test_cfg, mock_flex_contract, mock_autopay_contract, mock_token_contract
 # ):
-#     """Test reporting dai/usd on mumbai."""
-#     async with TelliotCore(config=mumbai_test_cfg) as core:
-#         # get PubKey and PrivKey from config files
+#     """Test reporting DIVA Protocol pool response on mumbai."""
+#     async with TelliotCore(config=ropsten_test_cfg) as core:
 #         account = core.get_account()
-
+#         diva_feed = await assemble_diva_datafeed(
+#             pool_id=159, node=core.endpoint, account=account
+#         )
 #         flex = core.get_tellorflex_contracts()
 #         flex.oracle.address = mock_flex_contract.address
 #         flex.autopay.address = mock_autopay_contract.address
 #         flex.token.address = mock_token_contract.address
-
 #         flex.oracle.connect()
 #         flex.token.connect()
 #         flex.autopay.connect()
-
 #         # mint token and send to reporter address
 #         mock_token_contract.mint(account.address, 1000e18)
-
 #         # send eth from brownie address to reporter address for txn fees
 #         accounts[2].transfer(account.address, "1 ether")
-
 #         r = TellorFlexReporter(
 #             endpoint=core.endpoint,
 #             account=account,
@@ -39,14 +33,12 @@
 #             token=flex.token,
 #             autopay=flex.autopay,
 #             transaction_type=0,
-#             datafeed=dai_usd_median_feed,
+#             datafeed=diva_feed,
 #             max_fee=100,
+#             expected_profit="YOLO",
 #         )
-
 #         ORACLE_ADDRESSES = {mock_flex_contract.address}
-
 #         tx_receipt, status = await r.report_once()
-
 #         # Reporter submitted
 #         if tx_receipt is not None and status.ok:
 #             assert isinstance(tx_receipt, AttributeDict)
