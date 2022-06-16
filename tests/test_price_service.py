17b95d4d55103aea203a28cad4a68af27fdda244
oraclown <owen.burton13@gmail.com>
2022-06-16 19:55:49 +0200

    Add tests

---
+++ tests/test_price_service.py
@@ -0,0 +1,28 @@
+from unittest import mock
+
+import pytest
+import requests
+
+from telliot_core.pricing.price_service import WebPriceService
+
+
+@pytest.mark.asyncio
+async def test_webpriceservice_errors(caplog):
+    """ "Test failures of WebPriceService class"""
+
+    class FakePriceService(WebPriceService):
+        """Must implement get_price or NotImplementedError will be raised"""
+
+        async def get_price(self, asset, currency):
+            return None, None
+
+    def json_decode_error(*args, **kwargs):
+        """Override get method and raise JSONDecodeError"""
+        raise requests.exceptions.JSONDecodeError()
+
+    with mock.patch("requests.Session.get", side_effect=json_decode_error):
+        wsp = FakePriceService(name="FakePriceService", url="https://fakeurl.xyz")
+        result = wsp.get_url()
+
+        assert "error" in result
+        assert "JSON Decode Error" == result["error"]
