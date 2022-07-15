1a23f22812bc044651485f7cfc4187eb6382501a
oraclown <owen.burton13@gmail.com>
2022-07-15 12:12:14 -0700

    Rename, fix tests, add err handling, fix parser

--- src/telliot_feed_examples/sources/numeric_api_response.py
+++ src/telliot_feed_examples/sources/numeric_api_response.py
@@ -7,8 +7,8 @@ from typing import Union
 import requests
 
 from telliot_feed_examples.datasource import DataSource
-from telliot_feed_examples.dtypes.datapoint import DataPoint
 from telliot_feed_examples.dtypes.datapoint import datetime_now_utc
+from telliot_feed_examples.dtypes.datapoint import OptionalDataPoint
 from telliot_feed_examples.utils.log import get_logger
 
 
@@ -24,28 +24,15 @@ def api_call(url: str) -> Union[dict[Any, Any], Any]:
         logger.error(e)
         return None
 
-    files = r.json()
-    return files
-
-
-def find_value(key: str, json_dict: Optional[dict[Any, Any]]) -> list[Any]:
-    """Parse target numeric value from JSON blob."""
-    vals = []
-
-    def decode_dict(ex_dict: dict[Any, Any]) -> dict[Any, Any]:
-        """Nested search function"""
-        try:
-            vals.append(ex_dict[key])
-        except KeyError:
-            pass
-        return ex_dict
-
-    json.loads(json.dumps(json_dict, indent=1), object_hook=decode_dict)  # Return value ignored.
-    return vals
+    try:
+        return r.json()
+    except json.decoder.JSONDecodeError as e:
+        logger.error(e)
+        return None
 
 
 @dataclass
-class NumericApiResponseSource(DataSource[Any]):
+class NumericApiResponseSource(DataSource[float]):
     """data source for retrieving data from api calls"""
 
     #: URL to call and receive JSON dict to be parsed
@@ -54,29 +41,43 @@ class NumericApiResponseSource(DataSource[Any]):
     #: string of comma separated keys/list indices to parse value from JSON blob
     parseStr: str = ""
 
-    def main_parser(self) -> Union[list[Any], str]:
+    def main_parser(self) -> Optional[float]:
         """main method, parses key string and calls url to search returned json dict"""
-        # request url, handle exceptions
-        files = api_call(self.url)
-        json_obj = json.dumps(files, indent=1)
+        rsp = api_call(self.url)
+        if rsp is None:
+            logger.error("None returned from API call")
+            return None
 
-        if self.parseStr == "":
-            return json_obj
+        parse_list = [key.strip() for key in self.parseStr.split(",")]
+        parse_list = [int(key) if key.isdigit() else key for key in parse_list]
 
-        # create iterative list from parseStr
-        key_list = [key.strip() for key in self.parseStr.split(",")]
+        for key_index in parse_list:
+            try:
+                rsp = rsp[key_index]
+            except (IndexError, KeyError) as e:
+                logger.error(e)
+                return None
 
-        # find all vals that correspond with given keys, return them all as nested list
-        results = [find_value(key, files) for key in key_list]
-        results_fin = [val[0] if len(val) == 1 else val for val in results]
+        if isinstance(rsp, list) or isinstance(rsp, dict):
+            logger.error(f"Expecting a single float. Parsed response: {rsp}")
+            return None
 
-        return results_fin
+        if isinstance(rsp, float):
+            return rsp
+        elif isinstance(rsp, int):
+            return float(rsp)
+        else:
+            logger.error(f"Expecting a float. Parsed response type: {type(rsp)}")
+            return None
 
-    async def fetch_new_datapoint(self) -> DataPoint[Any]:
+    async def fetch_new_datapoint(self) -> OptionalDataPoint[Any]:
         """fetch new datapoint method, may need to overwrite return type"""
         val = self.main_parser()
+        if val is None:
+            logger.error("No value returned from API call")
+            return (None, None)
 
-        datapoint = (str(val), datetime_now_utc())
+        datapoint = (val, datetime_now_utc())
         self.store_datapoint(datapoint)
 
         logger.info(f"API info {datapoint[0]} retrieved at time {datapoint[1]}")
