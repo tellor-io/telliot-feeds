914a21be357644ed6ceb29566ec66caf89f7724f
oraclown <owen.burton13@gmail.com>
2022-07-18 12:17:20 -0700

    Fix test failing bc wrong query tag

--- src/telliot_feeds/feeds/__init__.py
+++ src/telliot_feeds/feeds/__init__.py
@@ -46,7 +46,7 @@ CATALOG_FEEDS = {
     "matic-usd-spot": matic_usd_median_feed,
     "usdc-usd-spot": usdc_usd_median_feed,
     "morphware-v1": morphware_v1_feed,
-    "gas-price-oracle": gas_price_oracle_feed,
+    "gas-price-oracle-example": gas_price_oracle_feed,
 }
 
 DATAFEED_BUILDER_MAPPING = {
