3fe8561c04da26f2a1995d647ebeb34edd3459eb
tallywiesenberg <t.j.wies@gmail.com>
2022-07-22 13:59:44 -0400

    feed + source

---
+++ src/telliot_feeds/feeds/snapshot_feed.py
@@ -0,0 +1,17 @@
+"""
+Datafeed for reporting responses to the GasPriceOracle query type.
+
+More info:
+https://github.com/tellor-io/dataSpecs/blob/main/types/GasPriceOracle.md
+
+"""
+from telliot_feeds.datafeed import DataFeed
+from telliot_feeds.queries.snapshot import Snapshot
+from telliot_feeds.sources.manual_input_source import ManualInputSource
+
+proposalId = None
+
+gas_price_oracle_feed = DataFeed(
+    query=Snapshot(proposalId=proposalId),
+    source=ManualInputSource(),
+)
