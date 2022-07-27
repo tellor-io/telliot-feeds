5c0d3edfc5871ddd9d716004d298ddd9da1f97f4
oraclown <owen.burton13@gmail.com>
2022-07-27 14:52:16 -0700

    Fetch pools from subgraph

--- tests/integrations/diva_protocol/test_get_pools.py
+++ tests/integrations/diva_protocol/test_get_pools.py
@@ -6,15 +6,35 @@ Filter out pools that have already been reported.
 Filter out pools that have already been settled.
 Filter out pools with unsupported reference assets or
 unsupported collateral tokens."""
-
 import pytest
+
+from telliot_feeds.integrations.diva_protocol.pools import fetch_from_subgraph
+from telliot_feeds.integrations.diva_protocol.pools import pools_query
+
+
+# centralized oracle ran by DIVA Protocol team
+DIVA_ORACLE = "0x245b8abbc1b70b370d1b81398de0a7920b25e7ca"
 
 
 @pytest.mark.asyncio
 async def test_get_pools_from_subgraph():
-    pass
+    query = pools_query(
+        last_id=49100,
+        data_provider=DIVA_ORACLE,
+        expiry_since=1658845000,
+    )
+    pools = await fetch_from_subgraph(
+        query=query,
+        network="ropsten",
+    )
 
-
-@pytest.mark.asyncio
-async def test_fail_get_pools_from_subgraph():
-    pass
+    # print(pools)
+    assert pools is not None
+    assert isinstance(pools, list)
+    assert len(pools) > 0
+    assert isinstance(pools[0], dict)
+    assert "id" in pools[0]
+    assert "referenceAsset" in pools[0]
+    assert "collateralToken" in pools[0]
+    assert "collateralBalance" in pools[0]
+    assert "expiryTime" in pools[0]
