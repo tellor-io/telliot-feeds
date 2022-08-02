376fd709e814ca3b08a5703435a4998f671a493d
akrem <akremstudy@gmail.com>
2022-08-02 09:36:44 -0400

    manual spot price source

---
+++ tests/sources/test_spot_price_manual_source.py
@@ -0,0 +1,13 @@
+from curses.ascii import SP
+import pytest
+
+from telliot_feeds.sources.manual_sources.spot_price_input_source import SpotPriceManualSource
+from telliot_feeds.utils.log import get_logger
+
+logger = get_logger(__name__)
+
+@pytest.mark.asyncio
+async def test_manual_spot_price(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "1234")
+    result, _ = await SpotPriceManualSource().fetch_new_datapoint()
+    assert result == 1234
