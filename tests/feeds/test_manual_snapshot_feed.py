39773419cb064f65b3e8736e3a02c4a743e0bc07
oraclown <owen.burton13@gmail.com>
2022-08-02 10:28:21 -0500

    Format code

--- tests/feeds/test_manual_snapshot_feed.py
+++ tests/feeds/test_manual_snapshot_feed.py
@@ -1,5 +1,6 @@
-import pytest
 from unittest import mock
+
+import pytest
 
 from telliot_feeds.feeds.snapshot_feed import snapshot_manual_feed
 from telliot_feeds.utils.log import get_logger
@@ -17,7 +18,7 @@ async def test_valid_user_nput(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda: "y")
     (val, _) = await snapshot_manual_feed.source.fetch_new_datapoint()
     assert isinstance(val, bool)
-    assert val is True    
+    assert val is True
 
 
 @pytest.mark.asyncio
