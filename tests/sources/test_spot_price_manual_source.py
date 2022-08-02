5703cb4568f4a1d9a41901a358770c8870fa9605
akrem <akremstudy@gmail.com>
2022-08-02 09:40:39 -0400

    style

--- tests/sources/test_spot_price_manual_source.py
+++ tests/sources/test_spot_price_manual_source.py
@@ -1,4 +1,3 @@
-from curses.ascii import SP
 import pytest
 
 from telliot_feeds.sources.manual_sources.spot_price_input_source import SpotPriceManualSource
@@ -6,8 +5,9 @@ from telliot_feeds.utils.log import get_logger
 
 logger = get_logger(__name__)
 
+
 @pytest.mark.asyncio
 async def test_manual_spot_price(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: "1234")
+    monkeypatch.setattr("builtins.input", lambda _: "1234")
     result, _ = await SpotPriceManualSource().fetch_new_datapoint()
     assert result == 1234
