6bdc00a9660d7a9e2b200f150e35e222963847b4
oraclown <owen.burton13@gmail.com>
2022-08-12 08:28:19 -0500

    Ensure prices printed to console correctly

--- tests/sources/test_diva_manual_entry_source.py
+++ tests/sources/test_diva_manual_entry_source.py
@@ -7,11 +7,13 @@ from telliot_feeds.sources.manual_sources.diva_manual_source import DivaManualSo
 
 
 @pytest.mark.asyncio
-async def test_source():
+async def test_source(capsys):
     """Test retrieving DIVA Protocol query response from user input."""
     with mock.patch("builtins.input", side_effect=["-1234", "1234", "5678", ""]):
         source = DivaManualSource()
         val, dt = await source.fetch_new_datapoint()
+        captured = capsys.readouterr()
 
         assert isinstance(dt, datetime)
         assert val == [1234, 5678]
+        assert "[1234000000000000000000, 5678000000000000524288]" in captured.out
