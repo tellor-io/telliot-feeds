8f4a3038a8bd0d012d040b99afb861b52f42f627
oraclown <owen.burton13@gmail.com>
2022-08-17 08:38:41 -0500

    Format code, fix pytest method name

--- tests/funder/test_base_funder.py
+++ tests/funder/test_base_funder.py
@@ -1,9 +1,8 @@
 import pytest
-import os
+from chained_accounts import ChainedAccount
+from chained_accounts import find_accounts
 
 from telliot_feeds.funder.feed_funder import FeedFunder
-from chained_accounts import find_accounts
-from chained_accounts import ChainedAccount
 
 
 @pytest.fixture(scope="module", autouse=True)
@@ -14,7 +13,7 @@ def fake_account():
     if not accounts:
         fake_key = "0x57fe7105302229455bcfd58a8b531b532d7a2bb3b50e1026afa455cd332bf706"
         ChainedAccount.add(f"fake-{chain_id}", [chain_id], fake_key, password="")
-        accounts = find_accounts(chain_id=chain_id)[0]
+        accounts = find_accounts(chain_id=chain_id)
     acc = accounts[0]
     acc.unlock("")
     return acc
@@ -24,14 +23,14 @@ def test_feed_funder(fake_account):
     "Test FeedFunder class"
     feed_funder = FeedFunder(account=fake_account)
 
-    with pytest.riases(NotImplementedError):
+    with pytest.raises(NotImplementedError):
         feed_funder.get_feeds_to_fund()
 
-    with pytest.riases(NotImplementedError):
+    with pytest.raises(NotImplementedError):
         feed_funder.fund_feeds()
-    
-    with pytest.riases(NotImplementedError):
+
+    with pytest.raises(NotImplementedError):
         feed_funder.fund_feed()
-    
-    with pytest.riases(NotImplementedError):
+
+    with pytest.raises(NotImplementedError):
         feed_funder.alert_funds_exhausted()
