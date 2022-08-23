a6defacf8a2c5834ad0413dc65f4243baabf6bed
akrem <akremstudy@gmail.com>
2022-08-23 06:24:51 -0400

    add datafeed examples

--- src/telliot_feeds/feeds/snapshot_feed.py
+++ src/telliot_feeds/feeds/snapshot_feed.py
@@ -15,3 +15,7 @@ snapshot_manual_feed = DataFeed(
     query=Snapshot(proposalId=proposalId),
     source=ManualSnapshotInputSource(),
 )
+snapshot_feed_example = DataFeed(
+    query=Snapshot(proposalId="cce9760adea906176940ae5fd05bc007cc9252b524832065800635484cb5cb57"),
+    source=ManualSnapshotInputSource(),
+)
