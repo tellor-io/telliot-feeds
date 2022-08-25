b52eb4a09401f2e8ba2a05d48ee6b1f90443ce4f
oraclown <owen.burton13@gmail.com>
2022-08-25 18:03:17 -0500

    Add test specs

---
+++ tests/integrations/diva_protocol/test_cli.py
@@ -0,0 +1,24 @@
+
+
+from this import d
+
+
+def test_integrations_help_cmd():
+    # ensure diva cmd is available
+    pass
+
+
+def test_diva_help_cmd():
+    # ensure diva options (report / settle) are available
+    pass
+
+
+def test_report_cmd():
+    # telliot-feeds integrations diva report
+    pass
+
+
+def test_settle_cmd():
+    # telliot-feeds integrations diva settle --pool-id=<id>
+    pass
+
