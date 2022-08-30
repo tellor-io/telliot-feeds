6a288451410b9727c71248aa09ea0a083437fe6f
akrem <akremstudy@gmail.com>
2022-08-30 09:08:19 -0400

    track manualy built query id

--- src/telliot_feeds/reporters/tellorflex.py
+++ src/telliot_feeds/reporters/tellorflex.py
@@ -20,9 +20,8 @@ from telliot_feeds.feeds import CATALOG_FEEDS
 from telliot_feeds.feeds.matic_usd_feed import matic_usd_median_feed
 from telliot_feeds.feeds.trb_usd_feed import trb_usd_median_feed
 from telliot_feeds.reporters.interval import IntervalReporter
-from telliot_feeds.reporters.reporter_autopay_utils import (
-    autopay_suggested_report,
-)
+from telliot_feeds.reporters.reporter_autopay_utils import autopay_suggested_report
+from telliot_feeds.reporters.reporter_autopay_utils import CATALOG_QUERY_IDS
 from telliot_feeds.reporters.reporter_autopay_utils import get_feed_tip
 from telliot_feeds.utils.log import get_logger
 from telliot_feeds.utils.reporter_utils import tellor_suggested_report
@@ -249,6 +248,8 @@ class TellorFlexReporter(IntervalReporter):
         """Fetches datafeed suggestion plus the reward amount from autopay if query tag isn't selected
         if query tag is selected fetches the rewards, if any, for that query tag"""
         if self.datafeed:
+            if self.datafeed.query.query_id not in CATALOG_QUERY_IDS:
+                CATALOG_QUERY_IDS[self.datafeed.query.query_id] = self.datafeed.query.descriptor
             self.autopaytip = await self.rewards()
             return self.datafeed
 
