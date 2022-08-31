ae042330977411cadbf0a223745b0d83d92a80c7
oraclown <owen.burton13@gmail.com>
2022-08-31 10:55:05 -0500

    Don't create pools in constructor

--- contracts/integrations/DIVAProtocolMock.sol
+++ contracts/integrations/DIVAProtocolMock.sol
@@ -39,63 +39,63 @@ contract DIVAProtocolMock {
 
     // Add some fake pools
     constructor() {
-        Pool memory fakePool;
+        // Pool memory fakePool;
 
-        fakePool.referenceAsset = "ETH/USD";
-        fakePool.expiryTime = 1657349074;
-        fakePool.floor = 2000000000000000000000;
-        fakePool.inflection = 2000000000000000000000;
-        fakePool.cap = 4500000000000000000000;
-        fakePool.supplyInitial = 100000000000000000000;
-        fakePool.collateralToken = 0x867e53feDe91d27101E062BF7002143EbaEA3e30;
-        fakePool.collateralBalanceShortInitial = 50000000000000000000;
-        fakePool.collateralBalanceLongInitial = 50000000000000000000;
-        fakePool.collateralBalance = 214199598796389167516;
-        fakePool.shortToken = 0x91E75Aebda86a6B02d5510438f2981AC4Af1A44d;
-        fakePool.longToken = 0x945b1fA4DB6Fb1f8d3C7501968F6549C8c147D4e;
-        fakePool.finalReferenceValue = 0;
-        fakePool.statusFinalReferenceValue = Status.Open;
-        fakePool.redemptionAmountLongToken = 0;
-        fakePool.redemptionAmountShortToken = 0;
-        fakePool.statusTimestamp = 1647349398;
-        fakePool.dataProvider = 0xED6D661645a11C45F4B82274db677867a7D32675;
-        fakePool.redemptionFee = 2500000000000000;
-        fakePool.settlementFee = 500000000000000;
-        fakePool.capacity = 0;
+        // fakePool.referenceAsset = "ETH/USD";
+        // fakePool.expiryTime = 1657349074;
+        // fakePool.floor = 2000000000000000000000;
+        // fakePool.inflection = 2000000000000000000000;
+        // fakePool.cap = 4500000000000000000000;
+        // fakePool.supplyInitial = 100000000000000000000;
+        // fakePool.collateralToken = 0x867e53feDe91d27101E062BF7002143EbaEA3e30;
+        // fakePool.collateralBalanceShortInitial = 50000000000000000000;
+        // fakePool.collateralBalanceLongInitial = 50000000000000000000;
+        // fakePool.collateralBalance = 214199598796389167516;
+        // fakePool.shortToken = 0x91E75Aebda86a6B02d5510438f2981AC4Af1A44d;
+        // fakePool.longToken = 0x945b1fA4DB6Fb1f8d3C7501968F6549C8c147D4e;
+        // fakePool.finalReferenceValue = 0;
+        // fakePool.statusFinalReferenceValue = Status.Open;
+        // fakePool.redemptionAmountLongToken = 0;
+        // fakePool.redemptionAmountShortToken = 0;
+        // fakePool.statusTimestamp = 1647349398;
+        // fakePool.dataProvider = 0xED6D661645a11C45F4B82274db677867a7D32675;
+        // fakePool.redemptionFee = 2500000000000000;
+        // fakePool.settlementFee = 500000000000000;
+        // fakePool.capacity = 0;
 
-        pools[3] = fakePool;
+        // pools[3] = fakePool;
 
-        Pool memory fakePool2;
+        // Pool memory fakePool2;
 
-        fakePool2.referenceAsset = "BTC/USD";
-        fakePool2.expiryTime = 1654700353;
-        fakePool2.floor = 2000000000000000000000;
-        fakePool2.inflection = 2000000000000000000000;
-        fakePool2.cap = 4500000000000000000000;
-        fakePool2.supplyInitial = 100000000000000000000;
-        fakePool2.collateralToken = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
-        fakePool2.collateralBalanceShortInitial = 50000000000000000000;
-        fakePool2.collateralBalanceLongInitial = 50000000000000000000;
-        fakePool2.collateralBalance = 214199598796389167516;
-        fakePool2.shortToken = 0x91E75Aebda86a6B02d5510438f2981AC4Af1A44d;
-        fakePool2.longToken = 0x945b1fA4DB6Fb1f8d3C7501968F6549C8c147D4e;
-        fakePool2.finalReferenceValue = 0;
-        fakePool2.statusFinalReferenceValue = Status.Open;
-        fakePool2.redemptionAmountLongToken = 0;
-        fakePool2.redemptionAmountShortToken = 0;
-        fakePool2.statusTimestamp = 1647349398;
-        fakePool2.dataProvider = 0xED6D661645a11C45F4B82274db677867a7D32675;
-        fakePool2.redemptionFee = 2500000000000000;
-        fakePool2.settlementFee = 500000000000000;
-        fakePool2.capacity = 0;
+        // fakePool2.referenceAsset = "BTC/USD";
+        // fakePool2.expiryTime = 1654700353;
+        // fakePool2.floor = 2000000000000000000000;
+        // fakePool2.inflection = 2000000000000000000000;
+        // fakePool2.cap = 4500000000000000000000;
+        // fakePool2.supplyInitial = 100000000000000000000;
+        // fakePool2.collateralToken = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
+        // fakePool2.collateralBalanceShortInitial = 50000000000000000000;
+        // fakePool2.collateralBalanceLongInitial = 50000000000000000000;
+        // fakePool2.collateralBalance = 214199598796389167516;
+        // fakePool2.shortToken = 0x91E75Aebda86a6B02d5510438f2981AC4Af1A44d;
+        // fakePool2.longToken = 0x945b1fA4DB6Fb1f8d3C7501968F6549C8c147D4e;
+        // fakePool2.finalReferenceValue = 0;
+        // fakePool2.statusFinalReferenceValue = Status.Open;
+        // fakePool2.redemptionAmountLongToken = 0;
+        // fakePool2.redemptionAmountShortToken = 0;
+        // fakePool2.statusTimestamp = 1647349398;
+        // fakePool2.dataProvider = 0xED6D661645a11C45F4B82274db677867a7D32675;
+        // fakePool2.redemptionFee = 2500000000000000;
+        // fakePool2.settlementFee = 500000000000000;
+        // fakePool2.capacity = 0;
 
-        pools[10] = fakePool2;
+        // pools[10] = fakePool2;
     }
 
     // Add a new pool
     function addPool(
         uint256 _poolId,
-        string calldata _referenceAsset,
+        string memory _referenceAsset,
         uint256 _expiryTime,
         uint256 _floor,
         uint256 _inflection,
