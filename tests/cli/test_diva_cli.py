553817b28bea76daa52ead41e78200b60745c326
oraclown <owen.burton13@gmail.com>
2022-09-02 08:37:49 -0500

    Accept diva diamond & middleware addresses as input

--- tests/cli/test_diva_cli.py
+++ tests/cli/test_diva_cli.py
@@ -1,3 +1,29 @@
+from click.testing import CliRunner
+from telliot_feeds.cli.main import main as cli_main
+
+
+def test_invalid_diva_diamond_address():
+    """Test checking for invalid diva diamond address."""
+    runner = CliRunner()
+    result = runner.invoke(cli_main, ["report", "-dpt", "true", "-dda", "1234"])
+
+    assert result.exception
+    assert result.exit_code == 2
+
+    assert "Address must be a valid hex string" in result.stdout
+
+
+def test_invalid_diva_middleware_address():
+    """Test checking for invalid diva middleware address."""
+    runner = CliRunner()
+    result = runner.invoke(cli_main, ["report", "-dpt", "true", "-dma", "1234"])
+
+    assert result.exception
+    assert result.exit_code == 2
+
+    assert "Address must be a valid hex string" in result.stdout
+
+
 def test_integrations_help_cmd():
     # ensure diva cmd is available
     pass
