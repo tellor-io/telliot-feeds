68ad1d470e9ebed83e5ba8c31c670f99a27ee8ba
tallywiesenberg <t.j.wies@gmail.com>
2022-09-12 17:34:15 -0400

    getting inputs

--- src/telliot_feeds/utils/cfg.py
+++ src/telliot_feeds/utils/cfg.py
@@ -5,6 +5,7 @@ import click
 from chained_accounts import ChainedAccount
 from chained_accounts import find_accounts
 from telliot_core.apps.telliot_config import TelliotConfig
+from telliot_core.model.endpoints import RPCEndpoint
 
 
 def mainnet_config() -> Optional[TelliotConfig]:
@@ -30,8 +31,27 @@ def mainnet_config() -> Optional[TelliotConfig]:
 def setup_account(cfg:TelliotConfig, account_name: Optional[str]) -> TelliotConfig:
     """Setup account via CLI if not already configured"""
 
-    #check if configs are setup
-    #prompt the user to select a differnt chain id
+    choice = input(f"Chain_id is {cfg.main.chain_id}. Do you want to update it? (y/n)")
+
+    if choice.lower() == "y":
+        new_chain_id = input("Enter a new chain id")    # chck if confings are setup
+        cfg.main.chain_id = new_chain_id
+
+    config_is_setup = check_config(cfg)
+    # if configs are setup...
+         # print current settings of cfg
+    if config_is_setup:
+        click.echo(
+            f"Your current configuration...\n"
+            f"Chain id is {cfg.main.chain_id}\n"
+            f"Endpoint {cfg.get_endpoint()}\n"
+            f"Account {find_accounts(chain_id=cfg.main.chain_id)}"
+        )
+
+    choice = input("Do you want to change the Endpoints? (y/n)")
+
+    # if not..
+    
     #prompt the user to add a chained account
     #add...
         #account name
@@ -69,37 +89,72 @@ def setup_account(cfg:TelliotConfig, account_name: Optional[str]) -> TelliotConf
     pass
 
 
-def setup_endpoint(cfg:TelliotConfig, chain_id:int):
+def setup_endpoint(cfg:TelliotConfig, chain_id:int) -> TelliotConfig:
     """Setup Endpoints via CLI if not already configured"""
-    pass
+    # pass
     # if test config...
     #     check for and add the test endpoint
     # else...
     #     check if any endpoints available for the chain_id
+    found_endpoint = True
+    try:
+        endpoint = cfg.get_endpoint(chain_id)
+    except:
+        found_endpoint = False
+    
     #     print settings if there are endpoints available
-    #     if multiple are available...
-    #         choose one or overwritee
-    #     else if one is available
-    #         choose it or overwrite
-    #     else
-    #         prompt user for...
-    #             network name
-    #             testnet or mainnet
-    #             provider
-    #             rpc url
-    #             explorer url
+    if found_endpoint:
+        choice = input(
+            f"This endpoint is available for chain_id {chain_id}: {str(endpoint)}"
+            "Do you want to use it? (y/n)"
+            )
+
+        if choice.lower() == "y":
+
+            return cfg
+
+        if choice.lower == "n":
+
+            return set_endpoint(cfg, chain_id)
+
+    if not found_endpoint:
+
+        return set_endpoint(cfg, chain_id)
 
 
-    # if "INFURA_API_KEY" in endpoint.url:
-    #     endpoint.url = f'wss://mainnet.infura.io/ws/v3/{os.environ["INFURA_API_KEY"]}'
+def check_config(cfg: TelliotConfig) -> bool:
 
-
-def check_config(cfg: TelliotConfig):
-
-    #do they want the current chain id, endpint and account?
+    # do they want the current chain id, endpint and account?
     # if yes, proceed to reporting
     # if not, trigger above functions
 
-    # print current settings of cfg, does the user want to replace any?
-    click.echo(f"Your current config settings on chain_id {cfg.main.chain_id}: " + str(cfg.main))
-    # replace the ones the user wants to replace
+    try:
+        cfg.get_endpoint()
+    except:
+        return False
+    if find_accounts(chain_id=cfg.main.chain_id) is not None:
+        return True
+    else: 
+        return False
+
+
+
+
+def set_endpoint(cfg: TelliotConfig, chain_id:int):
+
+    network_name = input("Enter network name: ")
+    provider = input("Enter Provider: ")
+    rpc_url = input("Enter RPC url")
+    explorer_url = input("Enter Explorer url: ")
+
+    new_endpoint = RPCEndpoint(chain_id, network_name, provider, rpc_url, explorer_url)
+
+    cfg.endpoints.add(new_endpoint)
+
+    click.echo(f"{new_endpoint} added!")
+
+    return cfg
+
+
+def set_config():
+    pass
