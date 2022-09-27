406c9b4eaef5415b596b0fcb0494df50bbb1edeb
akrem <akremstudy@gmail.com>
2022-09-27 12:49:50 -0400

    add comments and clean up code based on feedback

--- src/telliot_feeds/reporters/tips/suggest_datafeed.py
+++ src/telliot_feeds/reporters/tips/suggest_datafeed.py
@@ -7,10 +7,10 @@ from telliot_core.utils.timestamp import TimeStamp
 
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.reporters.tips.funded_feeds.funded_feeds import FundedFeeds
-from telliot_feeds.reporters.tips.funded_feeds.multicall_autopay import MulticallAutopay
-from telliot_feeds.reporters.tips.listener.funded_feeds_filter import FundedFeedFilter
+from telliot_feeds.reporters.tips.listener.tip_listener_filter import TipListenerFilter
 from telliot_feeds.reporters.tips.listener.utils import get_sorted_tips
-from telliot_feeds.reporters.tips.onetime.one_time_tips import OneTimeTips
+from telliot_feeds.reporters.tips.multicall_functions.multicall_autopay import MulticallAutopay
+from telliot_feeds.reporters.tips.onetime.one_time_tips import get_funded_one_time_tips
 from telliot_feeds.utils.log import get_logger
 
 
@@ -19,25 +19,28 @@ logger = get_logger(__name__)
 
 # suggest a feed here not a query tag, because build feed portion
 # or check both mappings for type
-async def feed_suggestion(
+async def get_feed_and_tip(
     autopay: TellorFlexAutopayContract, current_timestamp: Optional[TimeStamp] = None
 ) -> Optional[Tuple[Optional[DataFeed[Any]], Optional[int]]]:
-    chain_id = autopay.node.chain_id
+    """Fetch feeds with their tip and filter to get a feed suggestion with the max tip
 
-    if chain_id in (137, 80001, 69, 1666600000, 1666700000, 421611, 42161):
-        assert isinstance(autopay, TellorFlexAutopayContract)
+    Args:
+    - autopay contract object
+    - current_timestamp
 
+    Returns:
+    - tuple of feed and tip amount
+    """
     if current_timestamp is None:
         current_timestamp = TimeStamp.now().ts
 
     multi_call = MulticallAutopay()
-    feed_filter = FundedFeedFilter()
+    listener_filter = TipListenerFilter()
 
-    one_time_tips = OneTimeTips(autopay=autopay)
-    funded_feeds = FundedFeeds(autopay=autopay, multi_call=multi_call, feed_filter=feed_filter)
+    funded_feeds = FundedFeeds(autopay=autopay, multi_call=multi_call, listener_filter=listener_filter)
 
     feed_tips = await funded_feeds.querydata_and_tip(current_time=current_timestamp)
-    onetime_tips = await one_time_tips.get_funded_one_time_tips()
+    onetime_tips = await get_funded_one_time_tips(autopay=autopay, filtr=listener_filter.qtype_name_in_registry)
 
     if not feed_tips and not onetime_tips:
         logger.info("No tips available in autopay")
@@ -45,17 +48,17 @@ async def feed_suggestion(
 
     sorted_tips = get_sorted_tips(feed_tips, onetime_tips)
     for suggestion in sorted_tips:
-        query_data = suggestion[0]
-        tip_amount = suggestion[1]
+        query_data, tip_amount = suggestion
         if tip_amount == 0:
             return None, None
 
-        datafeed = feed_filter.qtag_in_feed_mapping(query_data)
+        datafeed = listener_filter.qtag_in_feed_mapping(query_data)
+
         if datafeed is None:
-            datafeed = feed_filter.qtype_in_feed_builder_mapping(query_data)
+            datafeed = listener_filter.qtype_in_feed_builder_mapping(query_data)
 
         if datafeed is not None:
-            query = feed_filter.get_query_from_qtyp_name(query_data)
+            query = listener_filter.get_query_from_qtyp_name(query_data)
             datafeed.query = query
             break
 
--- src/telliot_feeds/reporters/tips/tip_amount.py
+++ src/telliot_feeds/reporters/tips/tip_amount.py
@@ -6,20 +6,25 @@ from telliot_core.utils.timestamp import TimeStamp
 
 from telliot_feeds.reporters.tips.listener.dtypes import QueryIdandFeedDetails
 from telliot_feeds.reporters.tips.listener.funded_feeds_filter import FundedFeedFilter
-from telliot_feeds.reporters.tips.selected_queryid.selected_queryid_multicall_autopay import AutopayMulticall
+from telliot_feeds.reporters.tips.multicall_functions.multicall_autopay import MulticallAutopay
 from telliot_feeds.utils.log import get_logger
 
 logger = get_logger(__name__)
-call = AutopayMulticall()
+call = MulticallAutopay()
 filtr = FundedFeedFilter()
 
 
 async def fetch_feed_tip(
     autopay: TellorFlexAutopayContract, query_id: bytes, timestamp: Optional[TimeStamp] = None
 ) -> int:
+    """Fetch tip amount for a given query id
+
+    Args:
+    - query_id
+
+    Returns: tip amount"""
     if timestamp is None:
         timestamp = TimeStamp.now().ts
-    """Fetch tip amount for a query id"""
     call.autopay = autopay
     month_old = int(timestamp - 2_592_000)
     tip_amount: int = 0
@@ -45,7 +50,7 @@ async def fetch_feed_tip(
         return tip_amount
 
     # filter out uneligible feeds
-    eligible_feeds = await filtr.filter_uneligible_feeds(timestamps_list_and_datafeed, timestamp)
+    eligible_feeds = await filtr.window_and_priceThreshold_unmet_filter(timestamps_list_and_datafeed, timestamp)
 
     if not eligible_feeds:
         logger.info("All feeds filtered out current timestamp not eligible for feed tip")
@@ -57,20 +62,21 @@ async def fetch_feed_tip(
     unclaimed_count, status = await call.rewards_claimed_status_call(feeds_with_timestamps_filtered)
 
     if not unclaimed_count:
-        tip_amount += tip(feeds_with_timestamps_filtered)
+        tip_amount += tip_sum(feeds_with_timestamps_filtered)
         return tip_amount
 
-    feeds_with_tips = filtr.get_real_balance(feeds_with_timestamps_filtered, unclaimed_count)
+    feeds_with_tips = filtr.calculate_true_feed_balance(feeds_with_timestamps_filtered, unclaimed_count)
 
     if not feeds_with_tips:
         return tip_amount
 
-    tip_amount += tip(feeds_with_tips)
+    tip_amount += tip_sum(feeds_with_tips)
 
     return tip_amount
 
 
-def tip(feeds: List[QueryIdandFeedDetails]) -> int:
+def tip_sum(feeds: List[QueryIdandFeedDetails]) -> int:
+    """Sum up the tip from all the feeds"""
     tip_total = 0
 
     for feed in feeds:
