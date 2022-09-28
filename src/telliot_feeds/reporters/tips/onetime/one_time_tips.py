f815324c4d756cbeb922fe539e5f1f1370b9ce40
akrem <akremstudy@gmail.com>
2022-09-28 08:28:38 -0400

    change from class to function

--- src/telliot_feeds/reporters/tips/onetime/one_time_tips.py
+++ src/telliot_feeds/reporters/tips/onetime/one_time_tips.py
@@ -1,42 +1,37 @@
 """Single tip feed suggeestion"""
-from typing import Dict
-from typing import List
+from typing import Callable
 from typing import Optional
-from typing import Tuple
 
 from telliot_core.tellor.tellorflex.autopay import TellorFlexAutopayContract
 
-from telliot_feeds.reporters.tips.listener.tip_listener_filter import TipListenerFilter
 from telliot_feeds.utils.log import get_logger
 
 logger = get_logger(__name__)
 
 
-class OneTimeTips(TipListenerFilter):
-    def __init__(self, autopay: TellorFlexAutopayContract) -> None:
-        self.autopay = autopay
+async def get_funded_one_time_tips(
+    autopay: TellorFlexAutopayContract, filtr: Callable[[bytes], bool]
+) -> Optional[dict[bytes, int]]:
+    """Trigger autopay call and filter response data
 
-    async def get_funded_one_time_tips(self) -> Optional[Dict[bytes, int]]:
-        """Trigger autopay call and filter response data
+    Return: list of tuples of only query data and tips
+    that exist in telliot registry
+    """
+    onetime_tips: Optional[list[tuple[bytes, int]]]
+    onetime_tips, status = await autopay.read("getFundedSingleTipsInfo")
 
-        Return: list of tuples of only query data and tips
-        that exist in telliot registry
-        """
-        onetime_tips: Optional[List[Tuple[bytes, int]]]
-        onetime_tips, status = await self.autopay.read("getFundedSingleTipsInfo")
+    if not status.ok or not onetime_tips:
+        logger.info("No one time tip funded queries available")
+        return None
 
-        if not status.ok or not onetime_tips:
-            logger.info("No one time tip funded queries available")
-            return None
+    for (query_data, reward) in list(onetime_tips):
+        if query_data == b"":
+            onetime_tips.remove((query_data, reward))
+            continue
 
-        for (query_data, reward) in list(onetime_tips):
-            if query_data == b"":
-                onetime_tips.remove((query_data, reward))
-                continue
+        if not filtr(query_data):
+            onetime_tips.remove((query_data, reward))
 
-            if not self.qtype_name_in_registry(query_data):
-                onetime_tips.remove((query_data, reward))
+    single_tips = {query_data: reward for (query_data, reward) in onetime_tips}
 
-        single_tips = {query_data: reward for (query_data, reward) in onetime_tips}
-
-        return single_tips
+    return single_tips
