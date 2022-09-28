a005eb564f6498e41cfd76df958af7c610a2356c
akrem <akremstudy@gmail.com>
2022-09-28 08:29:52 -0400

    remove inheritance, add comments, consolidate code

--- src/telliot_feeds/reporters/tips/listener/funded_feeds_filter.py
+++ src/telliot_feeds/reporters/tips/listener/funded_feeds_filter.py
@@ -1,8 +1,5 @@
 import math
-from typing import Dict
-from typing import List
 from typing import Optional
-from typing import Tuple
 
 from eth_abi import encode_single
 from eth_utils.conversions import to_bytes
@@ -14,14 +11,13 @@ from telliot_feeds.feeds import CATALOG_FEEDS
 from telliot_feeds.feeds import LEGACY_DATAFEEDS
 from telliot_feeds.reporters.tips import CATALOG_QUERY_IDS
 from telliot_feeds.reporters.tips.listener.dtypes import QueryIdandFeedDetails
-from telliot_feeds.reporters.tips.listener.tip_listener_filter import TipListenerFilter
 from telliot_feeds.utils.log import get_logger
 
 logger = get_logger(__name__)
 
 
-class FundedFeedFilter(TipListenerFilter):
-    def generate_ids(self, feed: QueryIdandFeedDetails) -> Tuple[bytes, bytes]:
+class FundedFeedFilter:
+    def generate_ids(self, feed: QueryIdandFeedDetails) -> tuple[bytes, bytes]:
         """Hash feed details to generate query id and feed id
 
         Return:
@@ -51,16 +47,19 @@ class FundedFeedFilter(TipListenerFilter):
         feed_start_timestamp: int,
         feed_window: int,
         feed_interval: int,
-    ) -> Tuple[bool, int]:
+    ) -> tuple[bool, int]:
         """
-        Calculates to check if timestamp(timestamp_to_check) is first in window
+        Checks if timestamp is first in window
+
+        Args:
+        - timestamp_before: the timestamp to check against
+        - timetamp_to_check: the timestamp thats checked if its first in window
+        - feed_start_timestamp
+        - feed_window
+        - feed_interval
 
         Return: bool
         """
-        # if all(i is None for i in [
-        #     timestamp_before, timestamp_to_check, feed_start_timestamp, feed_window, feed_interval
-        # ]):
-        #     return False, None
         # Number of intervals since start time
         num_intervals = math.floor((timestamp_to_check - feed_start_timestamp) / feed_interval)
         # Start time of latest submission window
@@ -70,7 +69,17 @@ class FundedFeedFilter(TipListenerFilter):
         return all(eligible), time_diff
 
     async def price_change(self, query_id: bytes, value_before: int) -> Optional[float]:
-        # check price threshold is met
+        """Check if priceThreshold is met
+
+        Args:
+        - query_id: used to get api source
+        - value_before: the value used to compare current value
+
+        Returns: float
+        """
+        if query_id not in CATALOG_QUERY_IDS:
+            return None
+
         query_tag = CATALOG_QUERY_IDS[query_id]
 
         if query_tag in CATALOG_FEEDS:
@@ -78,22 +87,26 @@ class FundedFeedFilter(TipListenerFilter):
         elif query_tag in LEGACY_DATAFEEDS:
             datafeed = LEGACY_DATAFEEDS[query_tag]
         else:
-            print("Can't fetch price for priceThreshold check")
+            logger.info(f"No Api source found for {query_tag} to check priceThreshold")
             return None
 
         value_now = await datafeed.source.fetch_new_datapoint()  # type: ignore
 
         if not value_now:
             note = f"Unable to fetch {datafeed} price for tip calculation"
-            error_status(note=note, log=logger.warning)
+            _ = error_status(note=note, log=logger.warning)
             return None
 
         value_now = value_now[0]
 
+        # catch price change up to 0.01 percent granularity
+        # could be done differently but this way is consistent with autopay contract
         if value_before == 0:
+            # if no before value set price change to 100 percent
             price_change = 10000
 
         elif value_now >= value_before:
+
             price_change = (10000 * (value_now - value_before)) / value_before
 
         else:
@@ -101,22 +114,24 @@ class FundedFeedFilter(TipListenerFilter):
 
         return price_change
 
-    def price_threshold_check_filter(self, feeds: List[QueryIdandFeedDetails]) -> List[QueryIdandFeedDetails]:
-        """Filter funded feeds based on threshold and telliot catalog feeds"""
-        for feed in list(feeds):
-            feed_id, query_id = self.generate_ids(feed)
-            feed.feed_id = feed_id
-            feed.query_id = query_id
-            tag = CATALOG_QUERY_IDS[query_id]
-            if feed.params.priceThreshold > 0 and (tag not in CATALOG_FEEDS or tag not in LEGACY_DATAFEEDS):
-                feeds.remove(feed)
-        return feeds
+    def api_support_check(self, feeds: list[QueryIdandFeedDetails]) -> list[QueryIdandFeedDetails]:
+        """Filter funded feeds where threshold is gt zero and no telliot catalog feeds support"""
+        telliot_supported_with_api = [
+            feed
+            for feed in feeds
+            if feed.params.priceThreshold == 0
+            or (feed.query_id in CATALOG_QUERY_IDS and CATALOG_QUERY_IDS[feed.query_id] in CATALOG_FEEDS)
+        ]
 
-    def timestamps_filter(self, feeds: List[QueryIdandFeedDetails]) -> List[QueryIdandFeedDetails]:
+        return telliot_supported_with_api
+
+    def timestamps_filter(self, feeds: list[QueryIdandFeedDetails]) -> list[QueryIdandFeedDetails]:
+        """list of timestamps compared against itself"""
         for feed in feeds:
+            # in case a query id has has none or too few to compare
             if len(feed.queryid_timestamps_list) < 2:
                 continue
-            for i, _ in enumerate(list(feed.queryid_timestamps_list)):
+            for i in range(len(feed.queryid_timestamps_list)):
                 in_eligibile_window = self.is_timestamp_first_in_window(
                     timestamp_before=feed.queryid_timestamps_list[i - 2],
                     timestamp_to_check=feed.queryid_timestamps_list[i - 1],
@@ -125,26 +140,42 @@ class FundedFeedFilter(TipListenerFilter):
                     feed_interval=feed.params.interval,
                 )
                 if not in_eligibile_window:
-                    # check price change
                     feed.queryid_timestamps_list.remove(feed.queryid_timestamps_list[i - 1])
         return feeds
 
-    def get_real_balance(
-        self, feeds: List[QueryIdandFeedDetails], unclaimed_timestamps_count: Dict[Tuple[bytes, bytes], int]
-    ) -> List[QueryIdandFeedDetails]:
-        # reduce balance based on unclaimed count of reported timestamps
+    def calculate_true_feed_balance(
+        self, feeds: list[QueryIdandFeedDetails], unclaimed_timestamps_count: dict[tuple[bytes, bytes], int]
+    ) -> list[QueryIdandFeedDetails]:
+        """Reduce balance based on unclaimed count of reported timestamps
+
+        Args:
+        - feeds: list of feeds
+        - unclaimed_timestamps_count: dict of queryid to unclaimed timestamps count
+
+        Returns: list of feeds
+        """
         for feed in list(feeds):
             key = (feed.feed_id, feed.query_id)
             unclaimed_count = unclaimed_timestamps_count[key]
             feed.params.balance -= feed.params.reward * unclaimed_count
-            # if remaining balance is zero filter out the feed from the dictionary
+            # if remaining balance is zero filter out the feed from the list
             if feed.params.balance <= 0:
                 feeds.remove(feed)
         return feeds
 
-    async def filter_uneligible_feeds(
-        self, feeds: List[QueryIdandFeedDetails], now_timestamp: TimeStamp
-    ) -> List[QueryIdandFeedDetails]:
+    async def window_and_priceThreshold_unmet_filter(
+        self, feeds: list[QueryIdandFeedDetails], now_timestamp: TimeStamp
+    ) -> list[QueryIdandFeedDetails]:
+
+        """Remove feeds from list that either submitting now won't be first in window
+        or price threshold is not met
+
+        Args:
+        - feeds: list of feeds
+        - now_timestamp: current timestamp used to check if first in window
+
+        Returns: list of feeds that could possibly reward a tip
+        """
         for feed in list(feeds):
             # check if your timestamp will be first in window for
             # this feed if not discard feed_details
@@ -166,6 +197,12 @@ class FundedFeedFilter(TipListenerFilter):
                 if balance < feed.params.reward:
                     feeds.remove(feed)
                 continue
+            elif price_threshold == 0 and not in_eligible_window:
+                # if now_timestamp is not in eligible window, discard funded feed
+                # remove feedId from feedId to queryId mapping
+                # discard funded feed
+                feeds.remove(feed)
+                continue
 
             if price_threshold != 0 and not in_eligible_window:
                 try:
@@ -184,11 +221,4 @@ class FundedFeedFilter(TipListenerFilter):
                     feeds.remove(feed)
                     continue
 
-            if price_threshold == 0:
-                if not in_eligible_window:
-                    # if now_timestamp is not in eligible window, discard funded feed
-                    # remove feedId from feedId to queryId mapping
-                    # discard funded feed
-                    feeds.remove(feed)
-                    continue
         return feeds
