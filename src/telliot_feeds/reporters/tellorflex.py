c681a24dd4f3335a7b427f406dabcdb7e150a9a7
akrem <akremstudy@gmail.com>
2022-10-14 01:03:50 -0400

    persist datafeed if qtag selected in cli

--- src/telliot_feeds/reporters/tellorflex.py
+++ src/telliot_feeds/reporters/tellorflex.py
@@ -72,6 +72,7 @@ class TellorFlexReporter(IntervalReporter):
         self.gas_price_speed = gas_price_speed
         self.autopaytip = 0
         self.staked_amount: Optional[float] = None
+        self.qtag_selected = False if self.datafeed is None else True
 
         logger.info(f"Reporting with account: {self.acct_addr}")
 
@@ -248,6 +249,7 @@ class TellorFlexReporter(IntervalReporter):
         """Fetches datafeed suggestion plus the reward amount from autopay if query tag isn't selected
         if query tag is selected fetches the rewards, if any, for that query tag"""
         if self.datafeed:
+            # add query id to catalog to fetch tip for legacy autopay
             if self.datafeed.query.query_id not in CATALOG_QUERY_IDS:
                 CATALOG_QUERY_IDS[self.datafeed.query.query_id] = self.datafeed.query.descriptor
             self.autopaytip = await self.rewards()
@@ -354,9 +356,15 @@ class TellorFlexReporter(IntervalReporter):
             status.ok = False
             status.error = "Estimated profitability below threshold."
             logger.info(status.error)
+            # reset datafeed for a new suggestion if qtag wasn't selected in cli
+            if self.qtag_selected is False:
+                self.datafeed = None
             return status
+        # reset autopay tip to check for tips again
         self.autopaytip = 0
-        self.datafeed = None
+        # reset datafeed for a new suggestion if qtag wasn't selected in cli
+        if self.qtag_selected is False:
+            self.datafeed = None
 
         return status
 
