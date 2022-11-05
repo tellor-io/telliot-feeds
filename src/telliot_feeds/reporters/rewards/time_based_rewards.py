a8a76754b667d14b8ec61e1f8cf95845991b0e86
akrem <akremstudy@gmail.com>
2022-11-05 02:09:14 -0400

    Fix TBR calculation

--- src/telliot_feeds/reporters/rewards/time_based_rewards.py
+++ src/telliot_feeds/reporters/rewards/time_based_rewards.py
@@ -1,5 +1,6 @@
 """Utilities for calculating time-based rewards (TBR)"""
 from telliot_core.tellor.tellor360.oracle import Tellor360OracleContract
+from telliot_core.utils.timestamp import TimeStamp
 
 from telliot_feeds.utils.log import get_logger
 
@@ -8,19 +9,28 @@ logger = get_logger(__name__)
 
 async def get_time_based_rewards(oracle: Tellor360OracleContract) -> int:
     """
-    Time based rewards, as of Tellor360,
-    are the TRB balance in the Oracle contract
-    minus the TRB staking rewards and
-    the totalStakeAmount
+    Reward that will be given if a reporter submits now:
+    TBR is calculated by factoring in the time of the last value to determine when it
+    started accruing and the total rewards balance available in the contract since thats
+    what will be dispersed if the reward is < than whats available in the contract
     """
-
     tbr: int
+    reward: int
 
-    tbr, status = await oracle.read("getTotalTimeBasedRewardsBalance")
+    time_of_last_new_value, last_val_status = await oracle.read("timeOfLastNewValue")
+    tbr, tbr_status = await oracle.read("getTotalTimeBasedRewardsBalance")
 
-    if status.ok:
-        return tbr
-
-    else:
-        logger.warning("Unable to calculate time-based rewards for reporter: " + status.error)
+    # if any call fails return 0 and a msg that tbr can't calc'd
+    if not tbr_status.ok or not last_val_status.ok:
+        error = tbr_status.error if not tbr_status.ok else last_val_status.error
+        logger.warning(f"Unable to calculate time-based rewards for reporter: {error}")
         return 0
+
+    reward = (
+        (TimeStamp.now().ts - time_of_last_new_value) * 5e17
+    ) / 300  # amount of TRB rewards released per 5 minutes
+
+    if tbr <= 0 and reward <= 0:
+        return 0
+
+    return reward if reward < tbr else tbr
