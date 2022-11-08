1e53a7e0426d94c10b1984f8377e6f5c81f24a30
akrem <akremstudy@gmail.com>
2022-11-08 11:27:08 -0500

    typing

--- src/telliot_feeds/integrations/diva_protocol/utils.py
+++ src/telliot_feeds/integrations/diva_protocol/utils.py
@@ -80,7 +80,7 @@ def find_most_profitable_pool(pools: list[DivaPool]) -> DivaPool:
     Returns:
         Pool with the highest profit.
     """
-    pass
+    pass  # type: ignore
 
 
 def get_reported_pools() -> Any:
