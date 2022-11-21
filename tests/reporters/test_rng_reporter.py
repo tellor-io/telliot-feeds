bbbfc81ec0a3df2d82698e354f9088f5fee744e3
oraclown <owen.burton13@gmail.com>
2022-11-21 08:18:24 -0600

    Fix fail source log capture

--- tests/reporters/test_rng_reporter.py
+++ tests/reporters/test_rng_reporter.py
@@ -1,8 +1,10 @@
 import pytest
 import pytest_asyncio
 from telliot_core.apps.core import TelliotCore
+from telliot_core.utils.response import ResponseStatus
 from web3.datastructures import AttributeDict
 
+from telliot_feeds.reporters.rng_interval import logger
 from telliot_feeds.reporters.rng_interval import RNGReporter
 
 
@@ -24,10 +26,11 @@ async def rng_reporter(mumbai_test_cfg, tellor_360):
 
 
 @pytest.mark.asyncio
-async def test_rng_reporter_submit_once(rng_reporter):
+async def test_report(rng_reporter):
     """Test reporting once to the Tellor playground on Rinkeby
     with three retries."""
     r = rng_reporter
+    r.wait_period = 0
 
     EXPECTED_ERRORS = {
         "Current addess disputed. Switch address to continue reporting.",
@@ -52,3 +55,49 @@ async def test_rng_reporter_submit_once(rng_reporter):
         assert not tx_receipt
         assert not status.ok
         assert status.error in EXPECTED_ERRORS
+
+
+@pytest.mark.asyncio
+async def test_missing_blockhash(rng_reporter, monkeypatch, caplog):
+    """Mock the datasource missing btc blockhash. Make sure still attemps to report after 3 retries."""
+    r = rng_reporter
+    r.wait_period = 0
+
+    # mock datasource failure
+    async def mock_no_val(*args, **kwargs):
+        logger.warning("bazinga")
+        return None, None
+
+    monkeypatch.setattr(
+        "telliot_feeds.sources.blockhash_aggregator.TellorRNGManualSource.fetch_new_datapoint", mock_no_val
+    )
+
+    # mock functions that don't need to be tested here
+    async def mock_ensure_staked(*args, **kwargs):
+        return True, ResponseStatus()
+
+    r.ensure_staked = mock_ensure_staked
+
+    def has_native_token(*args, **kwargs):
+        return True
+
+    r.has_native_token = has_native_token
+
+    async def mock_online(*args, **kwargs):
+        return True
+
+    r.is_online = mock_online
+
+    async def mock_check_reporter_lock(*args, **kwargs):
+        return ResponseStatus()
+
+    r.check_reporter_lock = mock_check_reporter_lock
+
+    async def mock_ensure_profitable(*args, **kwargs):
+        return ResponseStatus()
+
+    r.ensure_profitable = mock_ensure_profitable
+
+    await r.report(report_count=3)
+
+    assert caplog.text.count("bazinga") == 3
