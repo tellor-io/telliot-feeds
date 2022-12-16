c380a92907d88e435bb056aa9662a7a5b337626f
oraclown <owen.burton13@gmail.com>
2022-12-16 15:13:08 -0600

    Format code

--- src/telliot_feeds/feeds/rai_usd_feed.py
+++ src/telliot_feeds/feeds/rai_usd_feed.py
@@ -4,8 +4,8 @@ from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coinbase import CoinbaseSpotPriceSource
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
-from telliot_feeds.sources.price_aggregator import PriceAggregator
 from telliot_feeds.sources.price.spot.nomics import NomicsSpotPriceSource
+from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 
 rai_usd_median_feed = DataFeed(
