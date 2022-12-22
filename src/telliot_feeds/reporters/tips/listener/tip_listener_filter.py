b6498e48ab0030fb473e6eae32c5cd344ab3e47e
oraclown <owen.burton13@gmail.com>
2022-12-22 13:33:16 -0600

    Add janky skip of TellorRNG query type until source fixed

--- src/telliot_feeds/reporters/tips/listener/tip_listener_filter.py
+++ src/telliot_feeds/reporters/tips/listener/tip_listener_filter.py
@@ -43,7 +43,12 @@ class TipListenerFilter:
         Return: bool
         """
         qtyp_name = self.decode_typ_name(qdata)
-        return qtyp_name in Registry.registry
+        lis = []
+        for key in Registry.registry:
+            if qtyp_name != "TellorRNG":
+                lis.append(key)
+        return tuple(lis)
+        # return qtyp_name in Registry.registry
 
     def qtag_from_feed_catalog(self, qdata: bytes) -> Optional[str]:
         """Check if query tag for given query data is available in CATALOG_FEEDS
