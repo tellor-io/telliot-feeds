37b9f6ff7d7f922ad58f25fff1bcd73424c4e81f
akrem <akremstudy@gmail.com>
2022-12-29 10:16:35 -0500

    Fix chiado name spelling

--- src/telliot_feeds/reporters/tips/__init__.py
+++ src/telliot_feeds/reporters/tips/__init__.py
@@ -8,8 +8,8 @@ from telliot_feeds.queries.query_catalog import query_catalog
 # add testnet support for multicall that aren't avaialable in the package
 Network.PulsechainTestnet = 941
 MULTICALL2_ADDRESSES[Network.PulsechainTestnet] = "0x959a437F1444DaDaC8aF997E71EAF0479c810267"
-Network.Chaido = 10200
-MULTICALL3_ADDRESSES[Network.Chaido] = "0x08e08170712c7751b45b38865B97A50855c8ab13"
+Network.Chiado = 10200
+MULTICALL3_ADDRESSES[Network.Chiado] = "0x08e08170712c7751b45b38865B97A50855c8ab13"
 
 
 CATALOG_QUERY_IDS = {query_catalog._entries[tag].query.query_id: tag for tag in query_catalog._entries}
