0b227bb2d153a42b43b1c511a6c670925df87993
oraclown <owen.burton13@gmail.com>
2023-01-10 10:19:09 -0600

    Make mainnet web3 func

--- src/telliot_feeds/sources/blockhash_aggregator.py
+++ src/telliot_feeds/sources/blockhash_aggregator.py
@@ -23,15 +23,6 @@ from telliot_feeds.utils.log import get_logger
 logger = get_logger(__name__)
 
 
-ETHEREUM_CHAIN_ID = 1
-cfg = TelliotConfig()
-cfg.main.chain_id = ETHEREUM_CHAIN_ID
-try:
-    cfg.get_endpoint().connect()
-    w3 = cfg.get_endpoint().web3
-except ValueError:
-    w3 = None
-
 retry_strategy = Retry(
     total=3,
     backoff_factor=1,
@@ -39,6 +30,17 @@ retry_strategy = Retry(
     allowed_methods=["GET"],
 )
 adapter = HTTPAdapter(max_retries=retry_strategy)
+
+
+def get_mainnet_web3() -> Optional[Web3]:
+    """Get mainnet TelliotConfig."""
+    cfg = TelliotConfig()
+    cfg.main.chain_id = 1
+    try:
+        cfg.get_endpoint().connect()
+        return cfg.get_endpoint().web3
+    except ValueError:
+        return None
 
 
 def block_num_from_timestamp(timestamp: int) -> Optional[int]:
@@ -85,6 +87,7 @@ def block_num_from_timestamp(timestamp: int) -> Optional[int]:
 
 async def get_eth_hash(timestamp: int) -> Optional[str]:
     """Fetches next Ethereum blockhash after timestamp from API."""
+    w3 = get_mainnet_web3()
     if w3 is None:
         logger.warning("Web3 not connected")
         return None
