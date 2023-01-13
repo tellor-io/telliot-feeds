f3cdf0b51baf85a82a78e75879ca5a9dec3f1044
oraclown <owen.burton13@gmail.com>
2023-01-13 11:21:21 -0600

    Check for min num sources used

--- tests/feeds/test_eth_usd_feed.py
+++ tests/feeds/test_eth_usd_feed.py
@@ -6,12 +6,15 @@ from telliot_feeds.feeds.eth_usd_feed import eth_usd_median_feed
 
 
 @pytest.mark.asyncio
-async def test_AssetPriceFeed():
+async def test_eth_usd_median_feed(caplog):
     """Retrieve median ETH/USD price."""
     v, _ = await eth_usd_median_feed.source.fetch_new_datapoint()
 
     assert v is not None
     assert v > 0
+    assert (
+        "sources used in aggregate: 4" in caplog.text.lower() or "sources used in aggregate: 5" in caplog.text.lower()
+    )
     print(f"ETH/USD Price: {v}")
 
     # Get list of data sources from sources dict
