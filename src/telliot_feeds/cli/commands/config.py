2cc6e21d0ffb769166ea70d45011b75958a9de71
oraclown <owen.burton13@gmail.com>
2023-01-18 08:44:40 -0600

    Add update subcommand

--- src/telliot_feeds/cli/commands/config.py
+++ src/telliot_feeds/cli/commands/config.py
@@ -1,6 +1,7 @@
 import click
 import yaml
 from telliot_core.apps.telliot_config import TelliotConfig
+from telliot_feeds.utils.cfg import setup_config
 
 
 @click.group()
@@ -22,3 +23,21 @@ def show() -> None:
     state = cfg.get_state()
 
     print(yaml.dump(state, sort_keys=False))
+
+
+@click.option(
+    "--account",
+    "-a",
+    "account_str",
+    help="Name of account used for reporting, staking, etc. More info: run `telliot account --help`",
+    required=False,
+    nargs=1,
+    type=str,
+)
+@config.command()
+def update(
+        account_str: str,
+) -> None:
+    """Update configuration."""
+    _, _ = setup_config(None, account_str)
+    
