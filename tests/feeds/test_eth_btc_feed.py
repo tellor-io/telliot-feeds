c78203d5d457dd4e62821382afbb7025224fec3d
oraclown <owen.burton13@gmail.com>
2023-01-31 17:58:04 -0600

    Update names & test assert

--- tests/feeds/test_eth_btc_feed.py
+++ tests/feeds/test_eth_btc_feed.py
@@ -7,15 +7,15 @@ from telliot_feeds.feeds.eth_btc_feed import eth_btc_median_feed
 
 @pytest.mark.asyncio
 async def test_eth_btc_median_feed(caplog):
-    """Retrieve median ETH/USD price."""
+    """Retrieve median ETH/BTC price."""
     v, _ = await eth_btc_median_feed.source.fetch_new_datapoint()
 
     assert v is not None
     assert v > 0
-    assert (
-        "sources used in aggregate: 4" in caplog.text.lower() or "sources used in aggregate: 5" in caplog.text.lower()
+    assert (  # sometimes only 3 sources are used bc binance restricts locations
+        "sources used in aggregate: 4" in caplog.text.lower() or "sources used in aggregate: 3" in caplog.text.lower()
     )
-    print(f"ETH/USD Price: {v}")
+    print(f"ETH/BTC Price: {v}")
 
     # Get list of data sources from sources dict
     source_prices = [source.latest[0] for source in eth_btc_median_feed.source.sources if source.latest[0]]
