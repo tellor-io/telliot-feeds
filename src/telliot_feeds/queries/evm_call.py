4fd8070f2136d04184b7816e5da62f994a99ab81
oraclown <owen.burton13@gmail.com>
2023-02-02 21:08:24 -0600

    Do some hacky query data adjustment

--- src/telliot_feeds/queries/evm_call.py
+++ src/telliot_feeds/queries/evm_call.py
@@ -3,8 +3,10 @@ Subclass AbiQuery to implement the EVMCall query type.
 """
 from dataclasses import dataclass
 
-from telliot_feeds.queries.abi_query import AbiQuery
+from eth_abi import encode_abi
+
 from telliot_feeds.dtypes.value_type import ValueType
+from telliot_feeds.queries.abi_query import AbiQuery
 
 
 @dataclass
@@ -21,19 +23,38 @@ class EVMCall(AbiQuery):
     """
 
     chainId: int
-    contractAddress: str
-    calldata: str
+    contractAddress: bytes
+    calldata: bytes
 
     abi = [
         {"type": "uint256", "name": "chainId"},
         {"type": "address", "name": "contractAddress"},
-        {"type": "bytes", "name": "calldata"},
+        {"type": "bytes4", "name": "calldata"},
     ]
 
     @property
     def value_type(self) -> ValueType:
         """Expected response type.
 
-            `bytes` - the encoded value and timestamp
+        `bytes` - the encoded value and timestamp
         """
         return ValueType(abi_type="bytes", packed=False)
+
+    @property
+    def query_data(self) -> bytes:
+        """Encode the query type and parameters to create the query data.
+
+        This method uses ABI encoding to encode the query's parameter values.
+        """
+        param_values = [getattr(self, p["name"]) for p in self.abi]
+        param_types = [p["type"] for p in self.abi]
+        encoded_params = encode_abi(param_types, param_values)
+
+        q_data = encode_abi(["string", "bytes"], [type(self).__name__, encoded_params])
+        # The function selector (calldata) parameter must be shifted over, so that
+        # the query data matches the generated query data in Solidity.
+        last_32_bytes = q_data[-32:]
+        func_selector = last_32_bytes[:4]
+        shifted_func_selector = func_selector.rjust(32, b"\x00")
+        adjusted_q_data = q_data[:-32] + shifted_func_selector
+        return adjusted_q_data
