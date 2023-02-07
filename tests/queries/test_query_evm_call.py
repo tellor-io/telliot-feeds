f1c9b31dc7712d2ef9e8b6d449c9d16e7a114ef2
oraclown <owen.burton13@gmail.com>
2023-02-06 21:34:55 -0600

    Add steps for testing call & decode/encode val

--- tests/queries/test_query_evm_call.py
+++ tests/queries/test_query_evm_call.py
@@ -78,3 +78,57 @@ def test_evm_call_get_query_from_data():
     q2 = EVMCall.get_query_from_data(q.query_data)
     assert q2.query_data == q.query_data
     assert isinstance(q2, EVMCall)
+
+
+def test_encode_decode_reported_val():
+    _ = EVMCall(
+        chainId=1,
+        contractAddress="0x88dF592F8eb5D7Bd38bFeF7dEb0fBc02cf3778a0",
+        calldata=b"\x18\x16\x0d\xdd",
+    )
+    pass
+    # expected response type is bytes
+    # example response of encoded uint256:
+    # print(q.value_type.encode(420 * 10 ** 18))
+    # print(420 * 10 ** 18)
+    # encoded_int = encode_abi(["uint256"], [420 * 10 ** 18])
+    # assert encoded_int == bytes.fromhex("000000000000000000000000000000000000000000000016c4abbebea0100000")
+    # assert q.value_type.encode(encoded_int) == expected_rsp
+    # assert q.value_type.decode(expected_rsp) == encoded_int
+
+    # So in solidity you're assuming that the response will be encoded immediately to bytes
+    # or else we'd need to know the type of the response to decode it
+
+    # To do:
+    # 1. fix the dataspec query data & id
+    # 2. deploy a contract fixture, get its address and calldata for one of its read functions
+    # 3. create instance of EVMCall query with the address and calldata
+    # 4. call the function using the calldata and address like so:
+    # from web3 import Web3
+
+    # # Connect to an Ethereum client
+    # w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+
+    # # The contract address
+    # contract_address = '0x1234567890123456789012345678901234567890'
+
+    # # The function signature of the read function (can be obtained using a tool like Remix)
+    # function_signature = '0x12345678'
+
+    # # The arguments to pass to the read function
+    # arguments = [42]
+
+    # # Encode the arguments using `Web3.codec.encode_abi`
+    # encoded_arguments = w3.codec.encode_abi(['uint256'], arguments)
+
+    # # Combine the function signature and the encoded arguments to form the calldata
+    # calldata = function_signature + encoded_arguments[2:]
+
+    # # Call the contract's read function using `Web3.eth.call`
+    # result = w3.eth.call(
+    #     {'to': contract_address, 'data': calldata},
+    #     'latest'
+    # )
+
+    # 5. try encoding / decoding "result" with the query instance
+    # 6. Rewrite steps above as a datasouce and datafed, test separately
