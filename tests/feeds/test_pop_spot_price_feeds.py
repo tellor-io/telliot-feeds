f83c47d322fa897762743c415558d876cfc7306d
oraclown <owen.burton13@gmail.com>
2023-02-10 17:45:38 -0600

    Add test for new feeds

---
+++ tests/feeds/test_pop_spot_price_feeds.py
@@ -0,0 +1,60 @@
+"""Tests for these spot price feeds:
+
+AAVE/USD
+AVAX/USD
+BADGER/USD
+BCH/USD
+COMP/USD
+CRV/USD
+DOGE/USD
+DOT/USD
+EUL/USD
+FIL/USD
+GNO/USD
+LINK/USD
+LTC/USD
+SHIB/USD
+UNI/USD
+USDT/USD
+YFI/USD
+"""
+import pytest
+
+from telliot_feeds.feeds import *
+# import DataFeed
+from telliot_feeds.datafeed import DataFeed
+from telliot_feeds.queries.price.spot_price import SpotPrice
+
+
+@pytest.mark.asyncio
+async def test_spot_price_feeds():
+    """Test popular spot price feeds."""
+    assets = [
+        "AAVE",
+        "AVAX",
+        "BADGER",
+        "BCH",
+        "COMP",
+        "CRV",
+        "DOGE",
+        "DOT",
+        "EUL",
+        "FIL",
+        "GNO",
+        "LINK",
+        "LTC",
+        "SHIB",
+        "UNI",
+        "USDT",
+        "YFI",
+    ]
+    for name, obj in globals().items():
+        if isinstance(obj, DataFeed) and isinstance(obj.query, SpotPrice):
+            if obj.query.currency.lower() == 'usd' and obj.query.asset.upper() in assets:
+                print(f"Testing {name}")
+                v, _ = await obj.source.fetch_new_datapoint()
+                assert v > 0
+                
+
+        
+
