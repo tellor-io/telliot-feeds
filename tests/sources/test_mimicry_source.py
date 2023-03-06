a32043f82322609751c775f9135c82ccfef649de
akrem <akremstudy@gmail.com>
2023-03-06 10:02:59 -0500

    Add test mimicry source (skipped- requires key)

--- tests/sources/test_mimicry_source.py
+++ tests/sources/test_mimicry_source.py
@@ -1,6 +1,7 @@
 import pytest
 
 from telliot_feeds.sources.mimicry.mimicry import MimicryCollectionStatSource
+from telliot_feeds.sources.mimicry.mimicry_mashup_source import NFTMashupSource
 
 
 @pytest.mark.skip("See issue 603")
@@ -19,3 +20,27 @@ async def test_fetching_data():
     val, _ = await source.fetch_new_datapoint()
     print(val)
     assert isinstance(val, float)
+
+
+@pytest.mark.skip("Requires API key")
+@pytest.mark.asyncio
+async def test_fetching_nft_mashup_mcap():
+    """Test mimicry market cap mashup"""
+
+    source = NFTMashupSource(
+        metric="market-cap",
+        currency="usd",
+        collections=(
+            ("ethereum-mainnet", "0x50f5474724e0ee42d9a4e711ccfb275809fd6d4a"),
+            ("ethereum-mainnet", "0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d"),
+            ("ethereum-mainnet", "0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258"),
+        ),
+        tokens=(
+            ("ethereum-mainnet", "sand", "0x3845badAde8e6dFF049820680d1F14bD3903a5d0"),
+            ("ethereum-mainnet", "mana", "0x0F5D2fB29fb7d3CFeE444a200298f468908cC942"),
+            ("ethereum-mainnet", "ape", "0x4d224452801ACEd8B2F0aebE155379bb5D594381"),
+        ),
+    )
+    val, _ = await source.fetch_new_datapoint()
+    assert isinstance(val, int)
+    assert val > 0
