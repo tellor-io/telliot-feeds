1e6b7ab373b2b8b338cb4bc05cab08c769490b4f
akrem <akremstudy@gmail.com>
2023-03-29 08:06:36 -0400

    Fix style

--- tests/feeds/test_op_usd_feed.py
+++ tests/feeds/test_op_usd_feed.py
@@ -4,6 +4,7 @@ import pytest
 
 from telliot_feeds.feeds import op_usd_median_feed
 
+
 @pytest.mark.asyncio
 async def test_op_asset_price_feed():
     """Retrieve median OP/USD price."""
@@ -15,7 +16,6 @@ async def test_op_asset_price_feed():
 
     # Get list of data sources from sources dict
     source_prices = [source.latest[0] for source in op_usd_median_feed.source.sources if source.latest[0]]
-    
 
     # Make sure error is less than decimal tolerance
     assert (v - statistics.median(source_prices)) < 10**-6
