b9db04ed347e8479fe007a1236b61ff255d65fc2
Sinh Vu <khacsinh.vu.16091994@gmail.com>
2023-06-09 22:14:28 +0700

    add multicall for pulsechain

--- src/telliot_feeds/reporters/tips/__init__.py
+++ src/telliot_feeds/reporters/tips/__init__.py
@@ -64,6 +64,20 @@ add_multicall_support(
     multicall3_address="0x5c691b93A8A77D9a36C1Cc8e4e08a4283d1665e1",
 )
 
+add_multicall_support(
+    network="Pulsechain",
+    network_id=369,
+    state_override=False,
+    multicall3_address="0xcA11bde05977b3631167028862bE2a173976CA11",
+)
+
+add_multicall_support(
+    network="Pulsechain Testnet",
+    network_id=943,
+    state_override=False,
+    multicall3_address="0xcA11bde05977b3631167028862bE2a173976CA11",
+)
+
 CATALOG_QUERY_IDS = {query_catalog._entries[tag].query.query_id: tag for tag in query_catalog._entries}
 CATALOG_QUERY_DATA = {query_catalog._entries[tag].query.query_data: tag for tag in query_catalog._entries}
 # A list of query types that have a generic source that can take any properly formatted inputs and return a price
