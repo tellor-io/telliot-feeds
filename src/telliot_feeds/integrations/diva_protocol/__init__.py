42aa7442e8632951b0b406a47a15d79b53fb8e5c
oraclown <owen.burton13@gmail.com>
2023-06-14 07:23:52 -0500

    Add playground middleware contract addr

--- src/telliot_feeds/integrations/diva_protocol/__init__.py
+++ src/telliot_feeds/integrations/diva_protocol/__init__.py
@@ -5,5 +5,7 @@ SUPPORTED_HISTORICAL_PRICE_PAIRS = {
 SUPPORTED_COLLATERAL_TOKEN_SYMBOLS = {"dUSD"}
 DIVA_DIAMOND_ADDRESS = "0x2C9c47E7d254e493f02acfB410864b9a86c28e1D"
 DIVA_TELLOR_MIDDLEWARE_ADDRESS = "0x7950DB13cc37774614B0AA406e42a4C4f0BF26a6"
+# playgroud adapter (10 sec dispute period)
+# DIVA_TELLOR_MIDDLEWARE_ADDRESS = "0x0625855A4D292216ADEFA8043cDc69a6c99724C9"
 # for fetching pool data. network name is appended to this (e.g. "mumbai")
 BASE_SUBGRAPH_URL = "https://api.thegraph.com/subgraphs/name/divaprotocol/diva-protocol-v1-"
