283c50c3ae7f1d95f4b59414e7bc97240ea3d0fe
akrem <akremstudy@gmail.com>
2023-07-27 17:08:12 -0400

    tox

--- src/telliot_feeds/feeds/sweth_usd_feed.py
+++ src/telliot_feeds/feeds/sweth_usd_feed.py
@@ -3,9 +3,8 @@ from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
 from telliot_feeds.sources.price.spot.uniswapV3 import UniswapV3PriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
-from telliot_feeds.sources.sweth_source import swETHSpotPriceSource
-
 from telliot_feeds.sources.sweth_source import swETHMaverickSpotPriceSource
+from telliot_feeds.sources.sweth_source import swETHSpotPriceSource
 
 
 sweth_usd_median_feed = DataFeed(
