4b522c2435572d2c16bbe7eee5d2375c19d369bb
0xSpuddy <spuddyminer@gmail.com>
2023-11-08 14:27:49 -0500

    typo fixes

--- tests/feeds/test_wbeth_usd_feed.py
+++ tests/feeds/test_wbeth_usd_feed.py
@@ -6,14 +6,14 @@ from telliot_feeds.feeds.wbeth_usd_feed import wbeth_usd_median_feed
 
 
 @pytest.mark.asyncio
-async def test_cbeth_usd_median_feed(caplog):
+async def test_wbeth_usd_median_feed(caplog):
     """Retrieve median WBETH/USD price."""
     v, _ = await wbeth_usd_median_feed.source.fetch_new_datapoint()
 
     assert v is not None
     assert v > 0
     assert "sources used in aggregate: 2" in caplog.text.lower()
-    print(f"CBETH/USD Price: {v}")
+    print(f"WBETH/USD Price: {v}")
 
     # Get list of data sources from sources dict
     source_prices = [source.latest[0] for source in wbeth_usd_median_feed.source.sources if source.latest[0]]
