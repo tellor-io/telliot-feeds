49a3ec88791a072ef8ff97e701fdbe4f5b383d08
0xSpuddy <spuddyminer@gmail.com>
2023-12-06 10:46:10 -0500

    start testing

--- src/telliot_feeds/reporters/customized/conditional_reporter.py
+++ src/telliot_feeds/reporters/customized/conditional_reporter.py
@@ -50,3 +50,50 @@ class ConditionalReporter(Tellor360Reporter):
             logger.warning(f"error getting tellor data: {status.e}")
             return None
         return GetDataBefore(*data)
+    
+    async def conditions_met(self) -> bool:
+        """Trigger methods to check conditions if reporting spot is necessary
+
+        Returns:
+        - bool: True if conditions are met, False otherwise
+        """
+        logger.info("checking conditions and reporting if necessary")
+        if self.datafeed is None:
+            logger.debug(f"no datafeed was setß: {self.datafeed}. Please provide a spot-price query type (see --help)")
+            return False
+        tellor_data = await self.get_tellor_latest_data()
+        time = current_time()
+        time_passed_since_tellor_report = time - tellor_data.timestampRetrieved if tellor_data else time
+        if tellor_data is None:
+            logger.debug("tellor data returned None")
+            return True
+        elif not tellor_data.retrieved:
+            logger.debug(f"No oracle submissions in tellor for query: {self.datafeed.query.descriptor}")
+            return True
+        elif time_passed_since_tellor_report > self.stale_timeout:
+            logger.debug(f"tellor data is stale, time elapsed since last report: {time_passed_since_tellor_report}")
+            return True
+        else:
+            logger.info(f"tellor {self.datafeed.query.descriptor} data is recent enough")
+            return False
+
+    async def report(self, report_count: Optional[int] = None) -> None:
+        """Submit values to Tellor oracles on an interval."""
+
+        while report_count is None or report_count > 0:
+            online = await self.is_online()
+            if online:
+                if self.has_native_token():
+                    if await self.conditions_met():
+                        _, _ = await self.report_once()
+                    else:
+                        logger.info("feeds are recent enough, no need to report")
+
+            else:
+                logger.warning("Unable to connect to the internet!")
+
+            logger.info(f"Sleeping for {self.wait_period} seconds")
+            await asyncio.sleep(self.wait_period)
+
+            if report_count is not None:
+                report_count -= 1
