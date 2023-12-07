47400e90fbd74427fab564b48a670e046d95926e
0xSpuddy <spuddyminer@gmail.com>
2023-12-07 10:39:33 -0500

    asdf

--- src/telliot_feeds/reporters/customized/conditional_reporter.py
+++ src/telliot_feeds/reporters/customized/conditional_reporter.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from typing import Any
 from typing import Optional
 
+from telliot_feeds.feeds import DataFeed
 from telliot_feeds.reporters.tellor_360 import Tellor360Reporter
 from telliot_feeds.utils.log import get_logger
 from telliot_feeds.utils.reporter_utils import current_time
@@ -47,6 +48,44 @@ class ConditionalReporter(Tellor360Reporter):
             logger.warning(f"error getting tellor data: {status.e}")
             return None
         return GetDataBefore(*data)
+    
+    async def fetch_median_value(self) -> Optional[DataFeed[Any]]:
+        """Fetches datafeed
+
+        If the user did not select a query tag, there will have been no datafeed passed to
+        the reporter upon instantiation.
+        If the user uses the random feeds flag, the datafeed will be chosen randomly.
+        If the user did not select a query tag or use the random feeds flag, the datafeed will
+        be chosen based on the most funded datafeed in the AutoPay contract.
+
+        If the no-rewards-check flag is used, the reporter will not check profitability or
+        available tips for the datafeed unless the user has not selected a query tag or
+        used the random feeds flag.
+        """
+        # reset autopay tip every time fetch_datafeed is called
+        # so that tip is checked fresh every time and not carry older tips
+        self.autopaytip = 0
+        # TODO: This should be removed and moved to profit check method perhaps
+        if self.check_rewards:
+            # calculate tbr and
+            _ = await self.rewards()
+
+        if self.use_random_feeds:
+            self.datafeed = suggest_random_feed()
+
+        # Fetch datafeed based on whichever is most funded in the AutoPay contract
+        if self.datafeed is None:
+            suggested_feed, tip_amount = await get_feed_and_tip(self.autopay)
+
+            if suggested_feed is not None and tip_amount is not None:
+                logger.info(f"Most funded datafeed in Autopay: {suggested_feed.query.type}")
+                logger.info(f"Tip amount: {self.to_ether(tip_amount)}")
+                self.autopaytip += tip_amount
+
+                self.datafeed = suggested_feed
+                return self.datafeed
+
+        return self.datafeed
 
     async def conditions_met(self) -> bool:
         """Trigger methods to check conditions if reporting spot is necessary
@@ -61,6 +100,7 @@ class ConditionalReporter(Tellor360Reporter):
         tellor_data = await self.get_tellor_latest_data()
         time = current_time()
         time_passed_since_tellor_report = time - tellor_data.timestampRetrieved if tellor_data else time
+        tellor_value = tellor_data.value
         if tellor_data is None:
             logger.debug("tellor data returned None")
             return True
@@ -70,10 +110,15 @@ class ConditionalReporter(Tellor360Reporter):
         elif time_passed_since_tellor_report > self.stale_timeout:
             logger.debug(f"tellor data is stale, time elapsed since last report: {time_passed_since_tellor_report}")
             return True
-        tellor_value = tellor_data.value
+        eilf tellor_value
         else:
             logger.info(f"tellor {self.datafeed.query.descriptor} data is recent enough")
             return False
+
+
+
+
+
 
     async def report(self, report_count: Optional[int] = None) -> None:
         """Submit values to Tellor oracles on an interval."""
