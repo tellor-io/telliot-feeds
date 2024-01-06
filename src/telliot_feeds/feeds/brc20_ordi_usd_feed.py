4e7a435d3b822b32623a925b99da171ab8872bc2
0xSpuddy <sloetterle@tellor.io>
2024-01-05 21:18:01 -0500

    style fix

--- src/telliot_feeds/feeds/brc20_ordi_usd_feed.py
+++ src/telliot_feeds/feeds/brc20_ordi_usd_feed.py
@@ -1,7 +1,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.custom_price import CustomPrice
-from telliot_feeds.sources.price.spot.coingecko import CoinGeckoBRC20SpotPriceSource
 from telliot_feeds.sources.custom_price_aggregator import CustomPriceAggregator
+from telliot_feeds.sources.price.spot.coingecko import CoinGeckoBRC20SpotPriceSource
 
 ordi_usd_median_feed = DataFeed(
     query=CustomPrice(identifier="brc20", asset="ordi", currency="btc", unit=""),
