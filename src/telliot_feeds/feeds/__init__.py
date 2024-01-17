a6a82af11ee7503a3c12d01d44d956fd4870de92
0xSpuddy <sloetterle@tellor.io>
2024-01-17 14:37:38 -0500

    tests passing

--- src/telliot_feeds/feeds/__init__.py
+++ src/telliot_feeds/feeds/__init__.py
@@ -45,6 +45,7 @@ from telliot_feeds.feeds.landx_feed import wheat
 from telliot_feeds.feeds.link_usd_feed import link_usd_median_feed
 from telliot_feeds.feeds.ltc_usd_feed import ltc_usd_median_feed
 from telliot_feeds.feeds.matic_usd_feed import matic_usd_median_feed
+from telliot_feeds.feeds.meth_usd_feed import meth_usd_median_feed
 from telliot_feeds.feeds.mimicry.collection_stat_feed import mimicry_collection_stat_feed
 from telliot_feeds.feeds.mimicry.collection_stat_feed import mimicry_example_feed
 from telliot_feeds.feeds.mimicry.macro_market_mashup_feed import mimicry_mashup_example_feed
@@ -92,7 +93,6 @@ from telliot_feeds.feeds.wsteth_feed import wsteth_eth_median_feed
 from telliot_feeds.feeds.wsteth_feed import wsteth_usd_median_feed
 from telliot_feeds.feeds.xdai_usd_feed import xdai_usd_median_feed
 from telliot_feeds.feeds.yfi_usd_feed import yfi_usd_median_feed
-from telliot_feeds.feeds.meth_usd_feed import meth_usd_median_feed
 
 
 CATALOG_FEEDS: Dict[str, DataFeed[Any]] = {
--- src/telliot_feeds/feeds/meth_usd_feed.py
+++ src/telliot_feeds/feeds/meth_usd_feed.py
@@ -1,7 +1,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
-from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
 from telliot_feeds.sources.price.spot.agni import agniFinancePriceSource
+from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 
@@ -17,4 +17,3 @@ meth_usd_median_feed = DataFeed(
         ],
     ),
 )
-
