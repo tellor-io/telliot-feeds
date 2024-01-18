1c126b371c6edd6b7fbff2756fc308753001f86d
0xSpuddy <sloetterle@tellor.io>
2024-01-18 08:12:57 -0500

    coinpaprika source

--- src/telliot_feeds/feeds/meth_usd_feed.py
+++ src/telliot_feeds/feeds/meth_usd_feed.py
@@ -2,6 +2,7 @@ from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.agni import agniFinancePriceSource
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.coinpaprika import CoinpaprikaSpotPriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 
@@ -14,6 +15,7 @@ meth_usd_median_feed = DataFeed(
         sources=[
             CoinGeckoSpotPriceSource(asset="meth", currency="usd"),
             agniFinancePriceSource(asset="meth", currency="usd"),
+            CoinpaprikaSpotPriceSource(asset="meth-meth", currency="usd"),
         ],
     ),
 )
