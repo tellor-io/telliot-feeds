8b7c01fa31d253bda1a1e7e81398ab710b3ea5b6
0xSpuddy <sloetterle@tellor.io>
2024-05-29 14:47:33 -0400

    add fileCIDValue type

--- src/telliot_feeds/queries/fileCID.py
+++ src/telliot_feeds/queries/fileCID.py
@@ -1,14 +1,26 @@
-""" :mod:`telliot_feeds.queries.string_query`
-
-"""
 # Copyright (c) 2021-, Tellor Development Community
 # Distributed under the terms of the MIT License.
 from dataclasses import dataclass
+from typing import Any
 from typing import Optional
+
+from eth_abi import decode
+from eth_abi import encode
 
 from telliot_feeds.dtypes.value_type import ValueType
 from telliot_feeds.queries.abi_query import AbiQuery
 
+
+class CIDValueType(ValueType):
+    """A ValueType for CID Values."""
+
+    def encode(self, value: str) -> Any:
+        """Encode a string value."""
+        return encode([self.abi_type], [value])  # type: ignore
+
+    def decode(self, bytes_val: bytes) -> Any:
+        """Decode bytes into a string value."""
+        return decode([self.abi_type], bytes_val)  # type: ignore
 
 @dataclass
 class FileCID(AbiQuery):
@@ -27,6 +39,6 @@ class FileCID(AbiQuery):
     abi = [{"name": "url", "type": "string"}]
 
     @property
-    def value_type(self) -> ValueType:
+    def value_type(self) -> CIDValueType:
         """Returns a datatype for ipfs CIDs."""
         return ValueType(abi_type="string", packed=False)
