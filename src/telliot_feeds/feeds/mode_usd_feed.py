4b010641c9b22d5e37496e35b4f6d150e945ff07
0xSpuddy <sloetterle@tellor.io>
2024-06-03 13:41:55 -0400

    add cp

--- src/telliot_feeds/feeds/mode_usd_feed.py
+++ src/telliot_feeds/feeds/mode_usd_feed.py
@@ -1,6 +1,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.coinpaprika import CoinpaprikaSpotPriceSource
 from telliot_feeds.sources.price.spot.kimexchange import kimexchangePriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
@@ -12,6 +13,7 @@ mode_usd_median_feed = DataFeed(
         algorithm="median",
         sources=[
             CoinGeckoSpotPriceSource(asset="mode", currency="usd"),
+            CoinpaprikaSpotPriceSource(asset="mode-mode", currency="usd"),
             kimexchangePriceSource(asset="mode", currency="usd"),
         ],
     ),
