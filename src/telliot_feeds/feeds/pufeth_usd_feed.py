d64fad770c88c9faf10866e2a5c7012c80a039d0
0xSpuddy <sloetterle@tellor.io>
2024-08-29 12:22:50 -0400

    coinpaprika source

--- src/telliot_feeds/feeds/pufeth_usd_feed.py
+++ src/telliot_feeds/feeds/pufeth_usd_feed.py
@@ -1,6 +1,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.coinpaprika import CoinpaprikaSpotPriceSource
 from telliot_feeds.sources.price.spot.curvefiprice import CurveFiUSDPriceSource
 from telliot_feeds.sources.price.spot.uniswapV3 import UniswapV3PriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
@@ -14,6 +15,7 @@ pufeth_usd_median_feed = DataFeed(
         algorithm="median",
         sources=[
             CoinGeckoSpotPriceSource(asset="pufeth", currency="usd"),
+            CoinpaprikaSpotPriceSource(asset="pufeth-pufeth", currency="usd"),
             UniswapV3PriceSource(asset="pufeth", currency="usd"),
             CurveFiUSDPriceSource(asset="pufeth", currency="usd"),
         ],
