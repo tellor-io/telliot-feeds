4764e0e01f8b82a55ed956937aa9f16debf918ce
Jeff <95833205+4bra@users.noreply.github.com>
2024-09-26 19:09:20 -0400

    Update query_catalog.py

--- src/telliot_feeds/queries/query_catalog.py
+++ src/telliot_feeds/queries/query_catalog.py
@@ -659,3 +659,21 @@ query_catalog.add_entry(
     title="STONE/USD spot price",
     q=SpotPrice(asset="stone", currency="usd"),
 )
+
+query_catalog.add_entry(
+    tag="hex-usd-spot",
+    title="HEX/USD spot price",
+    q=SpotPrice(asset="hex", currency="usd"),
+)
+
+query_catalog.add_entry(
+    tag="inc-usd-spot",
+    title="INC/USD spot price",
+    q=SpotPrice(asset="inc", currency="usd"),
+)
+
+query_catalog.add_entry(
+    tag="plsx-usd-spot",
+    title="PLSX/USD spot price",
+    q=SpotPrice(asset="plsx", currency="usd"),
+)
