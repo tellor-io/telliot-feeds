e287251378625c247813f09a37985d0596d46283
EmanuellQA <emanuel@liquidloans.io>
2024-09-25 21:49:34 -0300

    added pulseX source and updated fetch-usd-query

--- src/telliot_feeds/feeds/fetch_usd_feed.py
+++ src/telliot_feeds/feeds/fetch_usd_feed.py
@@ -1,9 +1,11 @@
 """Datafeed for current price of FETCH in USD."""
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
-#from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
-#from telliot_feeds.sources.price.spot.pulsex_subgraph import PulseXSupgraphSource
+from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
+from telliot_feeds.sources.price.spot.pulsex_subgraph import PulseXSubgraphSource
 from telliot_feeds.sources.price.spot.fetch_usd_mock import FetchUsdMockSpotPriceSource
+from telliot_feeds.sources.price_aggregator import PriceAggregator
+from telliot_feeds.sources.price.spot.pulsex_fetch_dai import PulseXFETCHDAISource
 from dotenv import load_dotenv
 import os
 
@@ -13,14 +15,15 @@ if os.getenv("FETCH_USD_MOCK_PRICE"):
     fetch_usd_median_feed = DataFeed(
         query=SpotPrice(asset="fetch", currency="usd"),
         source=FetchUsdMockSpotPriceSource(asset="fetch", currency="usd")
-    )
-elif os.getenv("PULSEX_SUBGRAPH_URL") and os.getenv("FETCH_ADDRESS"):
-    fetch_usd_median_feed = DataFeed(
-        query=SpotPrice(asset="fetch", currency="usd"),
-        source=PulseXSupgraphSource(asset="fetch", currency="usd")
-    )
+        )
+        
 else:
     fetch_usd_median_feed = DataFeed(
         query=SpotPrice(asset="fetch", currency="usd"),
-        source=CoinGeckoSpotPriceSource(asset="fetch", currency="usd")
+        source=PriceAggregator(
+            asset="fetch",
+            currency="usd",
+            algorithm="median",
+            sources=[PulseXSubgraphSource(asset="fetch", currency="usd")],
+        )
     )
