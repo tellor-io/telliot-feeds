e6d00ce772c1ea564868196ffb1847e472bfebcf
EmanuellQA <emanuel@liquidloans.io>
2024-09-30 22:24:52 -0300

    update to pls feed to use testnet prices

--- src/telliot_feeds/feeds/pls_usd_feed.py
+++ src/telliot_feeds/feeds/pls_usd_feed.py
@@ -4,6 +4,7 @@ from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
 from telliot_feeds.sources.price.spot.coinpaprika import CoinpaprikaSpotPriceSource
 from telliot_feeds.sources.price.spot.uniswapV3 import UniswapV3PriceSource
+from telliot_feeds.sources.price.spot.pulsex_subgraph import PulseXSubgraphSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 pls_usd_median_feed = DataFeed(
@@ -13,9 +14,10 @@ pls_usd_median_feed = DataFeed(
         currency="usd",
         algorithm="median",
         sources=[
-            CoinGeckoSpotPriceSource(asset="pls", currency="usd"),
-            CoinpaprikaSpotPriceSource(asset="pls-pulsechain", currency="usd"),
-            UniswapV3PriceSource(asset="pls", currency="usd"),
+            #CoinGeckoSpotPriceSource(asset="pls", currency="usd"),
+            #CoinpaprikaSpotPriceSource(asset="pls-pulsechain", currency="usd"),
+            #UniswapV3PriceSource(asset="pls", currency="usd"),
+            PulseXSubgraphSource(asset="wpls", currency="usd"), #testnet wpls. Check pulsex_subgraph.py to confirm token address
         ],
     ),
 )
