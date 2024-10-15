22efba76d00daf85b42d85c35824829dd441160b
0xSpuddy <sloetterle@tellor.io>
2024-10-15 14:42:36 -0400

    tox

--- src/telliot_feeds/feeds/lsk_usd_feed.py
+++ src/telliot_feeds/feeds/lsk_usd_feed.py
@@ -1,7 +1,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
-from telliot_feeds.sources.price.spot.okx import OKXSpotPriceSource
 from telliot_feeds.sources.price.spot.kraken import KrakenSpotPriceSource
+from telliot_feeds.sources.price.spot.okx import OKXSpotPriceSource
 from telliot_feeds.sources.price.spot.uniswapV3 import UniswapV3PriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
@@ -14,7 +14,7 @@ lsk_usd_median_feed = DataFeed(
         sources=[
             OKXSpotPriceSource(asset="lsk", currency="usdt"),
             KrakenSpotPriceSource(asset="lsk", currency="usd"),
-            UniswapV3PriceSource(asset="lsk", currency="usd")
+            UniswapV3PriceSource(asset="lsk", currency="usd"),
         ],
     ),
 )
