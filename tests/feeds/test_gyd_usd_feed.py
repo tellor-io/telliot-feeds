e68692b1c4383fffeb11c354719ee0f575c6d774
0xSpuddy <sloetterle@tellor.io>
2025-03-07 13:23:04 -0500

    edit gyd test

--- tests/feeds/test_gyd_usd_feed.py
+++ tests/feeds/test_gyd_usd_feed.py
@@ -2,12 +2,13 @@ import statistics
 
 import pytest
 
+from telliot_feeds.sources.gyd_source import gydSpotPriceService
 from telliot_feeds.feeds.gyd_usd_feed import gyd_usd_median_feed
 
 
 @pytest.mark.asyncio
 async def test_gyd_usd_median_feed(caplog):
-    """Retrieve median SWETH/USD price."""
+    """Retrieve median GYD/USD price."""
     v, _ = await gyd_usd_median_feed.source.fetch_new_datapoint()
 
     assert v is not None
@@ -20,3 +21,16 @@ async def test_gyd_usd_median_feed(caplog):
 
     # Make sure error is less than decimal tolerance
     assert (v - statistics.median(source_prices)) < 10**-6
+
+async def test_0_response_from_balancer(caplog):
+    """Test that the GYD/USD feed returns None if the Balancer pools return 0"""
+    # Create a test-only version of the class that doesn't get registered
+    class TestGydSource:
+        asset: str = "gyd"
+        currency: str = "usd"
+        service: gydSpotPriceService = field(default_factory=gydSpotPriceService, init=False)
+
+    source = TestGydSource(asset="gyd", currency="usd")
+    v, _ = await source.fetch_new_datapoint()
+    assert v is None
+    assert "Balancer pool price is 0, returning None" in caplog.text.lower()
