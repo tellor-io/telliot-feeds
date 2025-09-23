865607ca864c381e676b53db22168f3af938cb33
0xspuddy <sloetterle@tellor.io>
2025-09-23 10:02:49 -0400

    tox fix

--- src/telliot_feeds/feeds/sfrxusd_usd_feed.py
+++ src/telliot_feeds/feeds/sfrxusd_usd_feed.py
@@ -1,9 +1,9 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
-from telliot_feeds.sources.price.spot.curvefiprice import CurveFiUSDPriceSource
 from telliot_feeds.sources.price.spot.coinmarketcap import CoinMarketCapSpotPriceSource
 from telliot_feeds.sources.price.spot.coinpaprika import CoinpaprikaSpotPriceSource
+from telliot_feeds.sources.price.spot.curvefiprice import CurveFiUSDPriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 sfrxusd_usd_median_feed = DataFeed(
