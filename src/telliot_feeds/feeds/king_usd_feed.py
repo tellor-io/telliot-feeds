dfc702ca96277824c4a793d1904a96f9c086ba6b
0xspuddy <sloetterle@tellor.io>
2025-10-17 10:50:33 -0400

    uniswapV4 source added for KING/USD

--- src/telliot_feeds/feeds/king_usd_feed.py
+++ src/telliot_feeds/feeds/king_usd_feed.py
@@ -1,7 +1,7 @@
 from telliot_feeds.datafeed import DataFeed
 from telliot_feeds.queries.price.spot_price import SpotPrice
 from telliot_feeds.sources.price.spot.coingecko import CoinGeckoSpotPriceSource
-from telliot_feeds.sources.price.spot.uniswapV3 import UniswapV3PriceSource
+from telliot_feeds.sources.price.spot.uniswapV4 import UniswapV4PriceSource
 from telliot_feeds.sources.price_aggregator import PriceAggregator
 
 
@@ -13,7 +13,7 @@ king_usd_median_feed = DataFeed(
         algorithm="median",
         sources=[
             CoinGeckoSpotPriceSource(asset="king", currency="usd"),
-            UniswapV3PriceSource(asset="king", currency="usd"),
+            UniswapV4PriceSource(asset="king", currency="usd"),
         ],
     ),
 )
